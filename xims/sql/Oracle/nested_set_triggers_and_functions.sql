5564cb44b9e93c37b02f67f56c1a41ee65fb13c7
Michael Kröll <pepl@cpan.org>
2003-02-25 15:52:55 +0000

    no temporary tables needed anymore! :-) testsuite integration is next...(after 6d vacation ;-)

--- xims/sql/Oracle/nested_set_triggers_and_functions.sql
+++ xims/sql/Oracle/nested_set_triggers_and_functions.sql
@@ -6,8 +6,8 @@
 CREATE OR REPLACE
 PACKAGE ci_util IS
     FUNCTION insert_into_tree(id_in IN NUMBER, lft_out OUT NUMBER, rgt_out OUT NUMBER) RETURN BOOLEAN;
-    FUNCTION move_tree(cleft_in IN NUMBER, crgt_in IN NUMBER, pleft_in IN NUMBER) RETURN BOOLEAN;
-    FUNCTION close_gap(lft_in IN NUMBER, rgt_in IN NUMBER) RETURN BOOLEAN;
+    FUNCTION move_tree(clft_in IN NUMBER, crgt_in IN NUMBER, parid_in IN NUMBER, clft_out OUT NUMBER, crgt_out OUT NUMBER) RETURN BOOLEAN;
+    FUNCTION close_gap(id_in IN NUMBER, lft_in IN NUMBER, rgt_in IN NUMBER) RETURN BOOLEAN;
 END ci_util;
 /
 
@@ -36,24 +36,44 @@ PACKAGE BODY ci_util IS
         RETURN TRUE;
     END;
 
-    FUNCTION move_tree(clft_in IN NUMBER, crgt_in IN NUMBER, parid_in IN NUMBER) RETURN BOOLEAN IS
+    FUNCTION move_tree(clft_in IN NUMBER, crgt_in IN NUMBER, parid_in IN NUMBER, clft_out OUT NUMBER, crgt_out OUT NUMBER) RETURN BOOLEAN IS
+    PRAGMA AUTONOMOUS_TRANSACTION;
+     CURSOR lft_cur IS
+         SELECT lft
+         FROM ci_documents
+         WHERE id = parid_in;
+     parent_lft NUMBER;
      leftbound NUMBER; rightbound NUMBER;
      treeshift NUMBER; cwidth NUMBER;
      leftrange NUMBER; rightrange NUMBER;
     BEGIN
-        IF clft_in > parid_in THEN 
-            treeshift  := parid_in - clft_in + 1;
-            leftbound  := parid_in + 1;
+        OPEN lft_cur;
+           FETCH lft_cur INTO parent_lft;
+        CLOSE lft_cur;
+
+        -- Parent cannot be underneath the child
+        IF parent_lft BETWEEN clft_in AND crgt_in THEN
+          RETURN FALSE;
+        END IF;
+
+        -- Child may already be in the proper place
+        IF clft_in = parent_lft+1 THEN
+          RETURN FALSE;
+        END IF;
+
+        IF clft_in > parent_lft THEN 
+            treeshift  := parent_lft - clft_in + 1;
+            leftbound  := parent_lft + 1;
             rightbound := clft_in - 1;
             cwidth     := crgt_in - clft_in + 1;
             leftrange  := crgt_in;
-            rightrange := parid_in;
+            rightrange := parent_lft;
         ELSE
-            treeshift  := parid_in - crgt_in;
+            treeshift  := parent_lft - crgt_in;
             leftbound  := crgt_in + 1;
-            rightbound := parid_in;
+            rightbound := parent_lft;
             cwidth     := clft_in - crgt_in - 1;
-            leftrange  := parid_in + 1;
+            leftrange  := parent_lft + 1;
             rightrange := clft_in;
         END IF;
 
@@ -66,12 +86,26 @@ PACKAGE BODY ci_util IS
                     WHEN rgt BETWEEN leftbound AND rightbound THEN rgt + cwidth
                     WHEN rgt BETWEEN clft_in AND crgt_in THEN rgt + treeshift
                     ELSE rgt END
-        WHERE lft < leftrange OR rgt > rightrange;
+        WHERE lft <> clft_in AND rgt <> crgt_in AND (lft < leftrange OR rgt > rightrange);
+
+        COMMIT;
+
+        IF clft_in BETWEEN leftbound AND rightbound THEN
+           clft_out := clft_in + cwidth;
+        ELSE
+           clft_out := clft_in + treeshift;
+        END IF;
+        IF crgt_in BETWEEN leftbound AND rightbound THEN
+           crgt_out := crgt_in + cwidth;
+        ELSE
+           crgt_out := crgt_in + treeshift;
+        END IF;
 
         RETURN TRUE;
     END;
 
-    FUNCTION close_gap(lft_in IN NUMBER, rgt_in IN NUMBER) RETURN BOOLEAN IS
+    FUNCTION close_gap(id_in IN NUMBER, lft_in IN NUMBER, rgt_in IN NUMBER) RETURN BOOLEAN IS
+    PRAGMA AUTONOMOUS_TRANSACTION;
     BEGIN
         UPDATE ci_documents 
           SET lft = CASE WHEN lft > lft_in
@@ -79,7 +113,9 @@ PACKAGE BODY ci_util IS
                          ELSE lft END,
               rgt = CASE WHEN rgt > rgt_in
                          THEN rgt - (rgt_in - lft_in + 1 )
-                         ELSE rgt END;
+                         ELSE rgt END
+          WHERE lft <> lft_in;
+        COMMIT;
         RETURN TRUE;
     END;
 
@@ -101,89 +137,32 @@ BEGIN
 END;
 /
 
-DROP TABLE ci_documents_mv_tmp;
-CREATE GLOBAL TEMPORARY TABLE ci_documents_mv_tmp (
-    clft    number,
-    crgt    number,
-    pid     number)
-ON COMMIT DELETE ROWS;
-/
-
-CREATE OR REPLACE TRIGGER ci_doc_lftrgt_before_upd
+CREATE OR REPLACE TRIGGER ci_doc_lftrgt_upd
 BEFORE UPDATE
 OF parent_id
 ON ci_documents
 REFERENCING NEW AS NEW OLD AS OLD
 FOR EACH ROW
-BEGIN
-    INSERT INTO ci_documents_mv_tmp (clft,crgt,pid)
-    VALUES (:OLD.lft,:OLD.rgt,:NEW.parent_id);
-END;
-/
-
-CREATE OR REPLACE TRIGGER ci_doc_lftrgt_after_upd
-AFTER UPDATE
-OF parent_id
-ON ci_documents
 DECLARE
---
---   PRAGMA AUTONOMOUS_TRANSACTION;
---
-    CURSOR tree_cur IS
-        SELECT clft,crgt,lft
-        FROM ci_documents_mv_tmp tmp, ci_documents d
-        WHERE tmp.pid = d.id;
-    tree_rec tree_cur%ROWTYPE;
     lv_return BOOLEAN;
 BEGIN
-    OPEN tree_cur;
-        FETCH tree_cur INTO tree_rec;
-        lv_return := ci_util.move_tree(tree_rec.clft, tree_rec.crgt, tree_rec.pid);
-    CLOSE tree_cur;
-    --DELETE FROM ci_documents_mv_tmp;
+    lv_return := ci_util.move_tree(:OLD.lft, :OLD.rgt, :NEW.parent_id, :NEW.lft, :NEW.rgt);
 END;
 /
 
-DROP TABLE ci_documents_del_tmp;
-CREATE GLOBAL TEMPORARY TABLE ci_documents_del_tmp (
-    lft    number,
-    rgt    number)
-ON COMMIT DELETE ROWS;
-/
-
-CREATE OR REPLACE TRIGGER ci_doc_lftrgt_before_del
+CREATE OR REPLACE TRIGGER ci_doc_lftrgt_del
 BEFORE DELETE
 ON ci_documents
 REFERENCING OLD AS OLD
 FOR EACH ROW
-BEGIN
-    INSERT INTO ci_documents_del_tmp (lft,rgt)
-    VALUES (:OLD.lft,:OLD.rgt);
-END;
-/
-
-CREATE OR REPLACE TRIGGER ci_doc_lftrgt_after_del
-AFTER DELETE
-ON ci_documents
 DECLARE
---
---   PRAGMA AUTONOMOUS_TRANSACTION;
---
-
-    CURSOR tree_cur IS
-        SELECT min(lft) lft, max(rgt) rgt
-        FROM ci_documents_del_tmp;
-    tree_rec tree_cur%ROWTYPE;
     lv_return BOOLEAN;
 BEGIN
-    OPEN tree_cur;
-        FETCH tree_cur INTO tree_rec;
-        lv_return := ci_util.close_gap(tree_rec.lft,
-                                       tree_rec.rgt
-                                      );
-    CLOSE tree_cur;
-    --DELETE FROM ci_documents_del_tmp;
+    lv_return := ci_util.close_gap(
+                                   :OLD.id,
+                                   :OLD.lft,
+                                   :OLD.rgt
+                                  );
 END;
 /
-
 
