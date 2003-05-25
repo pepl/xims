3e100b1f12ccf25b586fbea45eb88011e1594512
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2003-05-25 22:43:02 +0000

    (re)activate XSP object type - simple XSPs work inside default event, XSP errors are not catched and "Could not instantiate DP" error is raised

--- trunk/xims/lib/XIMS/XSPScript.pm
+++ trunk/xims/lib/XIMS/XSPScript.pm
@@ -1,16 +1,16 @@
 # Copyright (c) 2002-2003 The XIMS Project.
 # See the file "LICENSE" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+
 # $Id$
 package XIMS::XSPScript;
 
+use vars qw($VERSION @ISA);
 use strict;
-use vars qw( $VERSION @ISA );
+use XIMS::XML;
 
 $VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
-@ISA = ('XIMS::Document');
-
-use XIMS::Document;
+@ISA = ('XIMS::XML');
 
 ##
 #
@@ -32,8 +32,10 @@ sub new {
     my $class = ref( $proto ) || $proto;
     my %args = @_;
 
+
     $args{object_type_id} = 10 unless defined $args{object_type_id};
     $args{data_format_id} = 26 unless defined $args{data_format_id};
+
 
     return $class->SUPER::new( %args );
 }
