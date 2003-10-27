d123ddf97e9caa32ceb866d300f22f3b3904c514
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2003-10-27 21:14:04 +0000

    got rid of superflous whitespace and comments

--- trunk/xims/lib/XIMS/Document.pm
+++ trunk/xims/lib/XIMS/Document.pm
@@ -52,34 +52,25 @@ sub body {
 
     return $self->SUPER::body() unless $body;
 
-    if ( length $body ) {
-        # we only want the default XML-entities to be present
-        # this requires a patched version of HTML::Entities %entity2char :-/
-#warn "BODY - before entity decoding ", $body;
-        $body = XIMS::Entities::decode( $body );
-#warn "BODY - after entity decoding ", $body;
+    # we only want the default XML-entities to be present
+    $body = XIMS::Entities::decode( $body );
+    if ( $self->balanced_string( $body ) ) {
+        $self->SUPER::body( $body );
+        $retval = 1;
+    }
+    elsif ( not exists $args{dontbalance} ) {
+        XIMS::Debug( 3, "not well balanced; trying to balance it" );
+        $body = $self->balance_string( $body );
         if ( $self->balanced_string( $body ) ) {
             $self->SUPER::body( $body );
             $retval = 1;
         }
-        elsif ( not exists $args{dontbalance} ) {
-            XIMS::Debug( 3, "not well balanced; trying to balance it" );
-            $body = $self->balance_string( $body );
-            if ( $self->balanced_string( $body ) ) {
-#warn "BODY - after balancing: ", $body;
-                $self->SUPER::body( $body );
-                $retval = 1;
-            }
-            else {
-                XIMS::Debug( 2, "well balancing failed" );
-            }
-        }
         else {
-            XIMS::Debug( 2, "not well balanced; not trying to due to args" );
+            XIMS::Debug( 3, "well balancing failed" );
         }
     }
     else {
-        XIMS::Debug( 3, "wrong args" );
+        XIMS::Debug( 3, "not well balanced; not trying to due to args" );
     }
 
     return $retval;
