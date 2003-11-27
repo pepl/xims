f6fee5a7b7cd8aab81b4cbf83625c494da2a721c
Michael Kröll <pepl@cpan.org>
2003-11-27 13:22:51 +0000

    added target() - convenience method to get object referenced by sym_to_doc_id

--- xims/lib/XIMS/SymbolicLink.pm
+++ xims/lib/XIMS/SymbolicLink.pm
@@ -11,4 +11,17 @@ use XIMS::Object;
 $VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 @ISA = ('XIMS::Object');
 
+sub target {
+    my $self = shift;
+    my $target = shift;
+
+    if ( $target ) {
+        $self->symname_to_doc_id( $target->document_id() );
+        return $target;
+    }
+    else {
+        return XIMS::Object->new( document_id => $self->symname_to_doc_id() );
+    }
+}
+
 1;
