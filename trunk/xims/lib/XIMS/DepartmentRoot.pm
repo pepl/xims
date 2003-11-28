cea552e658c5642ca49620c266b35826267c1afa
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2003-11-28 11:22:50 +0000

    added add_departmentlinks() method (script using it 95% finished and coming soon) moved and cleane up the portlet handling logic from bin/departmentroot.pm

--- trunk/xims/lib/XIMS/DepartmentRoot.pm
+++ trunk/xims/lib/XIMS/DepartmentRoot.pm
@@ -6,8 +6,15 @@ package XIMS::DepartmentRoot;
 
 use strict;
 use vars qw( $VERSION @ISA );
+use XIMS::Object;
 use XIMS::Folder;
+use XIMS::Portlet;
+use XIMS::URLLink;
 use XIMS::DataFormat;
+use XIMS::Importer::Object;
+use XIMS::Importer::Object::URLLink;
+
+use XML::LibXML;
 
 $VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 @ISA = ('XIMS::Folder');
@@ -36,6 +43,213 @@ sub new {
     }
 
     return $class->SUPER::new( %args );
+}
+
+##
+# SYNOPSIS
+#    $self->add_departmentlinks( @objects );
+#
+# PARAMETER
+#    @objects    ... Array of content objects to be used as departmentlinks
+#
+# RETURNS
+#    undef on error, 1 on success
+#
+# DESCRIPTION
+#    Checks for an already assigned departmentlinks portlet and adds additional departmentlinks
+#    if such portlet already exists.  If not, a 'departmentlinks' folder is created,
+#    the departmentlinks and the portlet added and assigned respectively.
+#
+sub add_departmentlinks {
+    XIMS::Debug( 5, "called" );
+    my $self = shift;
+    my @objects = @_;
+
+    return undef unless @objects and scalar @objects > 0;
+
+    my $oimporter = XIMS::Importer::Object->new( User => $self->User, Parent => $self );
+
+    # hardcode alarm
+    my $dpl_location = 'departmentlinks';
+    my $dplp_location = 'departmentlinks_portlet';
+
+    # check for portlet
+    my @portlet_ids = $self->get_portlet_ids();
+
+    my $deptlinksportlet;
+    $deptlinksportlet = XIMS::Portlet->new( id => \@portlet_ids, location => $dplp_location ) if $portlet_ids[0];
+
+    my $deptlinksfolder = $deptlinksportlet->target() if $deptlinksportlet;
+    if ( not $deptlinksfolder ) {
+        # add folder if its not here already
+        $deptlinksfolder = $self->children( location => $dpl_location, marked_deleted => undef );
+        if ( not ($deptlinksfolder and $deptlinksfolder->id) ) {
+            my $deptlinksfolder = XIMS::Folder->new( User => $self->User, location => $dpl_location );
+            my $id = $oimporter->import( $deptlinksfolder );
+            if ( not $id ) {
+                XIMS::Debug( 2, "could not create departmentlinks folder '$dpl_location'" );
+                return undef;
+            }
+        }
+    }
+
+    # add links
+    my $urlimporter = XIMS::Importer::Object::URLLink->new( User => $self->User, Parent => $deptlinksfolder );
+    my $location_path;
+    foreach my $object ( @objects ) {
+        $location_path = XIMS::RESOLVERELTOSITEROOTS() eq '1' ? $object->location_path_relative() : $object->location_path();
+        my $urllink = XIMS::URLLink->new( User => $self->User(),
+                                          location => $location_path,
+                                          title => $object->title(),
+                                          abstract => $object->abstract(),
+                                       );
+        my $id = $urlimporter->import( $urllink );
+        XIMS::Debug( 3, "could not create departmentlink '$location_path'. Perhaps it already exists." ) unless $id;
+    }
+
+    # create and assign portlet if neccessary
+    if ( not $deptlinksportlet ) {
+        $deptlinksportlet = XIMS::Portlet->new( User => $self->User, location => $dplp_location );
+        $deptlinksportlet->target( $deptlinksfolder );
+        # *do not look at the following lines, hack-attack!*
+        # see portlet::generate_body why this hack is here.
+        # REWRITE!
+        my $body = '<content><column name="abstract"/><column name="location"/><column name="title"/><object-type name="URLLink"/></content>';
+        $deptlinksportlet->body( $body );
+        my $id = $oimporter->import( $deptlinksportlet );
+        if ( not $id ) {
+            XIMS::Debug( 2, "could not create departmentlinks portlet '$dplp_location'" );
+            return undef;
+        }
+        if ( not ($self->add_portlet( $deptlinksportlet ) and $self->update()) ) {
+            XIMS::Debug( 2, "could not assign departmentlinks portlet '$dplp_location'" );
+            return undef;
+        }
+    }
+
+    return 1;
+}
+
+sub get_portlet_ids {
+    XIMS::Debug( 5, "called" );
+    my $self = shift;
+
+    my @portlet_ids;
+
+    return undef unless ($self->body and length $self->body);
+
+    my $fragment = $self->_getbodyfragment();
+    return undef unless $fragment;
+
+    foreach my $node ( grep {$_->nodeName() eq 'portlet'} $fragment->childNodes ) {
+        push @portlet_ids, $node->string_value();
+    }
+
+    return undef unless (@portlet_ids and scalar @portlet_ids > 0);
+    return wantarray ? @portlet_ids : $portlet_ids[0];
+}
+
+
+##
+# SYNOPSIS
+#    $self->add_portlet( $target );
+#
+# PARAMETER
+#    $target ... XIMS::Object instance or location_path to one
+#
+# RETURNS
+#    undef on error, 1 on success
+#
+# DESCRIPTION
+#    Adds a portlet entry to the body in form of an XML-Fragment.
+#    Storing the portlets that way and not relational is something still
+#    to be discussed I guess.
+#
+sub add_portlet {
+    XIMS::Debug( 5, "called" );
+    my $self = shift;
+    my $target = shift;
+
+    return undef unless $target;
+
+    my $pobject = $target if (ref $target and $target->isa('XIMS::Object'));
+    $pobject ||= XIMS::Object->new( path => $target );
+    if ( $pobject ) {
+        my $id = $pobject->id;
+        XIMS::Debug( 4, "got portlet with id = $id ");
+        if ( defined $self->body and length $self->body ) {
+            XIMS::Debug( 4, "got existing department info" );
+
+            my $fragment = $self->_getbodyfragment();
+            return undef unless $fragment;
+
+            # check if id is already there
+            my ( $node ) = grep {$_->string_value() eq $id} $fragment->childNodes;
+            if ( defined $node ) {
+                XIMS::Debug( 3, "portlet ($id) already exists here" );
+                return 1;
+            }
+            else {
+                XIMS::Debug( 4, "3. step: insert the entry" );
+                $node = XML::LibXML::Element->new("portlet");
+                $node->appendText( $id );
+                $fragment->appendChild( $node );
+
+                XIMS::Debug( 4, "4. step: store body back." );
+                $self->body( $fragment->toString() );
+            }
+        }
+        else {
+            $self->body( "<portlet>$id</portlet>" );
+        }
+    }
+    else {
+        XIMS::Debug( "Portlet not found for $target!");
+        return undef;
+    }
+
+    return 1;
+}
+
+sub remove_portlet {
+    XIMS::Debug( 5, "called" );
+    my $self = shift;
+    my $portlet_id = shift;
+
+    return undef unless ($portlet_id and $portlet_id > 0);
+    return undef unless ($self->body and length $self->body);
+
+    my $fragment = $self->_getbodyfragment();
+    return undef unless $fragment;
+
+    my ( $node ) = grep {$_->string_value() eq $portlet_id} $fragment->childNodes;
+    if ( defined $node ) {
+        XIMS::Debug( 6, "found node" );
+        $node->unbindNode; # remove node from its context.
+        $self->body( $fragment->toString() );
+    }
+    else {
+        XIMS::Debug( 3, "no portlet with id '$portlet_id' found" );
+        return 0;
+    }
+
+    return 1;
+}
+
+sub _getbodyfragment {
+    my $self = shift;
+
+    my $parser = XML::LibXML->new();
+    my $fragment;
+    eval {
+        $fragment = $parser->parse_xml_chunk( $self->body );
+    };
+    if ( $@ ) {
+        XIMS::Debug( 2, "problem with the department body ($@)"  );
+        return undef;
+    }
+
+    return $fragment;
 }
 
 1;
