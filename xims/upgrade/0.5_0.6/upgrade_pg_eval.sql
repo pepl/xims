c9c1e849d79c9b7f33832e0cb75bc21415577d2d
Michael Kröll <pepl@cpan.org>
2004-01-13 10:40:03 +0000

    fixed insert object type statement, added insertion of data formats

--- xims/upgrade/0.5_0.6/upgrade_pg_eval.sql
+++ xims/upgrade/0.5_0.6/upgrade_pg_eval.sql
@@ -1,19 +1,35 @@
 \connect xims
 
 -- Add two new object types: Questionnaire and TAN_List
-INSERT INTO ci_object_types VALUES ('Questionnaire',0,0,1,1);
-INSERT INTO ci_object_types VALUES ('TAN_List',0,0,1,0);
+\echo inserting into ci_object_types...
+INSERT INTO ci_object_types ( id, name, is_fs_container, is_xims_data, redir_to_self, publish_gopublic )
+       VALUES ( nextval('ci_object_types_id_seq'), 'Questionnaire', 1, 1, 0, 0 );
+INSERT INTO ci_object_types ( id, name, is_fs_container, is_xims_data, redir_to_self, publish_gopublic )
+       VALUES ( nextval('ci_object_types_id_seq'), 'TAN_List', 1, 1, 0, 0 );
+
+-- Add two new data formats: Questionnaire and TAN_List
+\echo inserting into ci_data_formats...
+INSERT INTO ci_data_formats ( id, name, mime_type )
+       VALUES ( nextval('ci_data_formats_id_seq'), 'Questionnaire', 'text/xml' );
+INSERT INTO ci_data_formats ( id, name, mime_type, suffix )
+       VALUES ( nextval('ci_data_formats_id_seq'), 'TAN_List', 'text/plain', 'tls' );
 
 
 -- Add table for the questionnaire results
-CREATE TABLE ci_questionnaire_results (
-  document_id		INTEGER		NOT NULL, 
-  tan			VARCHAR(50)	NOT NULL, 
-  question_id		VARCHAR(50) 	NOT NULL, 
-  answer		TEXT, 
-  answer_timestamp	TIMESTAMP	DEFAULT now()	NOT NULL, 
-  id			SERIAL		PRIMARY KEY, 
-  CONSTRAINT fk_document_id FOREIGN KEY (document_id) REFERENCES ci_documents (id) ON DELETE RESTRICT ON UPDATE RESTRICT NOT DEFERRABLE INITIALLY IMMEDIATE
+\echo creating table 'ci_questionnaire_results'
+CREATE TABLE ci_questionnaire_results
+  (document_id      INTEGER      NOT NULL
+                                 REFERENCES ci_documents ( id )
+                                 ON DELETE RESTRICT
+                                 ON UPDATE RESTRICT
+                                 NOT DEFERRABLE
+                                 INITIALLY IMMEDIATE
+
+  ,tan              VARCHAR(50)  NOT NULL
+  ,question_id      VARCHAR(50)  NOT NULL
+  ,answer           TEXT
+  ,answer_timestamp TIMESTAMP    DEFAULT now()    NOT NULL
+  ,id               SERIAL       PRIMARY KEY
 );
 REVOKE ALL ON TABLE ci_questionnaire_results FROM PUBLIC;
 GRANT ALL ON TABLE ci_questionnaire_results TO xims;
@@ -21,7 +37,8 @@ GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE ci_questionnaire_results TO ximsru
 
 --functions as compatability-wrappers for Oracles vs. PostgreSQL
 CREATE FUNCTION ci_questionnaire_results_id_seq_nval() RETURNS INTEGER
-	AS 'BEGIN
-	      RETURN nextval(''ci_questionnaire_results_id_seq'');
-	    END;'
-	LANGUAGE 'plpgsql';
+    AS 'BEGIN
+          RETURN nextval(\'ci_questionnaire_results_id_seq\');
+        END;'
+    LANGUAGE 'plpgsql'
+;
