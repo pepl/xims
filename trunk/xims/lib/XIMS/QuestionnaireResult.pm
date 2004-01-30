4bb7f5a13daee9e2007098cae2e1dd5a6dba9c37
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2004-01-30 20:28:18 +0000

    adaptions for lc'ed fetch_select result keys

--- trunk/xims/lib/XIMS/QuestionnaireResult.pm
+++ trunk/xims/lib/XIMS/QuestionnaireResult.pm
@@ -10,7 +10,6 @@ use vars qw($VERSION @Fields @ISA);
 $VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 @ISA = qw( XIMS::AbstractClass );
 
-use Data::Dumper;
 use Text::Iconv;
 
 sub resource_type {
@@ -19,30 +18,6 @@ sub resource_type {
 
 sub fields {
     return @Fields;
-}
-
-sub new {
-    my $proto = shift;
-    my $class = ref( $proto ) || $proto;
-    my %args = @_;
-
-    my $self = bless {}, $class;
-
-    if ( scalar( keys(%args)) > 0 ) {
-        if ( ( defined( $args{id} ) ) || ( defined( $args{owner_id}) and defined( $args{stdhome} )) ) {
-            my $real_rslt = $self->data_provider->getQuestionnaireResult( %args );
-            if ( defined( $real_rslt )) {
-               $self->data( %{$real_rslt} );
-            }
-            else {
-                return undef;
-            }
-        }
-        else {
-            $self->data( %args );
-        }
-    }
-    return $self;
 }
 
 BEGIN {
@@ -57,7 +32,7 @@ sub store {
     my $self = shift;
     $self->answer_timestamp( $self->data_provider->db_now() );
     $self->id();
-	$self->answer( _decode( $self->answer() ) );
+    $self->answer( _decode( $self->answer() ) );
     my $id = $self->data_provider->createQuestionnaireResult( $self->data());
     $self->id( $id );
     return $id;
@@ -85,52 +60,52 @@ sub update {
 #    count of valid answered questionnaires
 #    count of invalid answered questionnaires
 sub get_result_count {
-  my $self = shift;
-  my $questionnaire_id = shift;
-  my $last_question = shift; # number of the last question that has to be answered (= total number of questions)
-  my $sql = "SELECT count(*) AS c1 FROM (SELECT tan FROM ci_questionnaire_results WHERE document_id = $questionnaire_id GROUP BY tan )AS sub1";
-  my $total_count = $self->data_provider->{Driver}->{dbh}->fetch_one_value( sql => $sql );
-  my $sql = "SELECT count(*) AS c1 FROM (SELECT tan FROM ci_questionnaire_results WHERE document_id = $questionnaire_id AND question_id = $last_question GROUP BY tan) AS sub1";
-  my $valid_count = $self->data_provider->{Driver}->{dbh}->fetch_one_value( sql => $sql );	
-#  XIMS::Debug (6, "#### ".Dumper( $valid_count ) );
-  return ($total_count, $valid_count, $total_count - $valid_count);
+    my $self = shift;
+    my $questionnaire_id = shift;
+    my $last_question = shift; # number of the last question that has to be answered (= total number of questions)
+    my $sql = "SELECT count(*) AS c1 FROM (SELECT tan FROM ci_questionnaire_results WHERE document_id = $questionnaire_id GROUP BY tan )AS sub1";
+    my $total_count = $self->data_provider->{Driver}->{dbh}->fetch_one_value( sql => $sql );
+    my $sql = "SELECT count(*) AS c1 FROM (SELECT tan FROM ci_questionnaire_results WHERE document_id = $questionnaire_id AND question_id = $last_question GROUP BY tan) AS sub1";
+    my $valid_count = $self->data_provider->{Driver}->{dbh}->fetch_one_value( sql => $sql );
+    #  XIMS::Debug (6, "#### ".Dumper( $valid_count ) );
+    return ($total_count, $valid_count, $total_count - $valid_count);
 }
 
 sub get_answer_count {
-  my $self = shift;
-  my $questionnaire_id = shift;
-  my $question_id = shift;
-  my $answer = shift;
-  my $sql = "SELECT count(*) FROM ci_questionnaire_results WHERE document_id = $questionnaire_id AND question_id = '$question_id' AND answer='$answer' ";
-  my $answer_count = $self->data_provider->{Driver}->{dbh}->fetch_one_value( sql => $sql );
-#  XIMS::Debug (6, "#### ".Dumper( $answer_count ) );
-  return $answer_count;
+    my $self = shift;
+    my $questionnaire_id = shift;
+    my $question_id = shift;
+    my $answer = shift;
+    my $sql = "SELECT count(*) FROM ci_questionnaire_results WHERE document_id = $questionnaire_id AND question_id = '$question_id' AND answer='$answer' ";
+    my $answer_count = $self->data_provider->{Driver}->{dbh}->fetch_one_value( sql => $sql );
+    #  XIMS::Debug (6, "#### ".Dumper( $answer_count ) );
+    return $answer_count;
 }
 
 sub get_answers {
-  my $self = shift;
-  my $questionnaire_id = shift;
-  my $question_id = shift;
-  my $sql = "SELECT answer, count(answer) AS count FROM ci_questionnaire_results WHERE document_id = $questionnaire_id AND question_id = '$question_id' GROUP BY answer";
-  my $answers = $self->data_provider->{Driver}->{dbh}->fetch_select( sql => $sql );
-  foreach my $answer_text ( @{$answers} ) {
-	${$answer_text}{'ANSWER'} = _encode( ${$answer_text}{'ANSWER'} );
-  }
-  return $answers;
+    my $self = shift;
+    my $questionnaire_id = shift;
+    my $question_id = shift;
+    my $sql = "SELECT answer, count(answer) AS count FROM ci_questionnaire_results WHERE document_id = $questionnaire_id AND question_id = '$question_id' GROUP BY answer";
+    my $answers = $self->data_provider->{Driver}->{dbh}->fetch_select( sql => $sql );
+    foreach my $answer_text ( @{$answers} ) {
+    ${$answer_text}{'answer'} = _encode( ${$answer_text}{'answer'} );
+    }
+    return $answers;
 }
 
 sub _encode {
-  my $string = shift;
-  my $converter = Text::Iconv->new( XIMS::DBENCODING(), "UTF-8" );
-  $string = $converter->convert($string) if defined $string;
-  return $string;
+    my $string = shift;
+    my $converter = Text::Iconv->new( XIMS::DBENCODING(), "UTF-8" );
+    $string = $converter->convert($string) if defined $string;
+    return $string;
 }
 
 sub _decode {
-  my $string = shift;
-  my $converter = Text::Iconv->new( "UTF-8", XIMS::DBENCODING() );
-  $string = $converter->convert($string) if defined $string;
-  return $string;
+    my $string = shift;
+    my $converter = Text::Iconv->new( "UTF-8", XIMS::DBENCODING() );
+    $string = $converter->convert($string) if defined $string;
+    return $string;
 }
 
 1;
