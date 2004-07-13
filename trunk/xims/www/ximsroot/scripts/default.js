cc66c84e62bafa61f9496687f2e21d6684161049
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2004-07-13 22:01:34 +0000

    javascript functions for client-side searchterm highlightning

--- trunk/xims/www/ximsroot/scripts/default.js
+++ trunk/xims/www/ximsroot/scripts/default.js
@@ -26,3 +26,70 @@ function openDocWindow(topic) {
     docWindow = window.open( "http://xims.info/documentation/users/xims-user_s-reference.sdbk#" + escape(topic), "displayWindow","resizable=yes,scrollbars=yes,width=800,height=480,screenX=100,screenY=300" );
 }
 
+function getParamValue( param ) {
+    var objQuery = new Object();
+    var objQuery2 = new Object();
+    var windowURL;
+    var strQuery = location.search.substring(1);
+    var aryQuery = strQuery.split(";");
+    var aryQuery2 = strQuery.split("&");
+    var pair = [];
+    var pair2 = [];
+    for (var i = 0; i < aryQuery.length; i++) {
+        pair = aryQuery[i].split("=");
+        if (pair.length == 2) {
+            objQuery[unescape(pair[0])] = unescape(pair[1]);
+        }
+    }
+    for (var i = 0; i < aryQuery2.length; i++) {
+        pair2 = aryQuery2[i].split("=");
+        if (pair2.length == 2) {
+            objQuery2[unescape(pair2[0])] = unescape(pair2[1]);
+        }
+    }
+
+    var hls = objQuery[param] ? objQuery[param] : objQuery2[param];
+    return hls;
+}
+
+var highlighted = false;
+function stringHighlight( mystring ) {
+    if (highlighted) {
+        return;
+    }
+    mystringRE = new RegExp('('+mystring+')', 'gi');
+    var body = document.getElementById("body");
+    content = body.innerHTML;
+    content = content.replace(mystringRE, '<span name="highlighted" style="background: yellow">$1</span>');
+    body.innerHTML = content;
+
+    highlighted = true
+}
+
+function toggleHighlight( hls ) {
+    var cssText;
+    if ( highlighted == false ) {
+        cssText = 'background: yellow';
+        highlighted = true;
+    }
+    else {
+        cssText = '';
+        highlighted = false;
+    }
+
+    if ( document.all ) {
+        var els = document.getElementsByTagName("span");
+        var i;
+        for(i = 0;i<els.length;i++){
+            if ( els[i].name == 'highlighted' ) {
+                els[i].style.cssText = cssText;
+            }
+        }
+    }
+    else {
+        var els = document.getElementsByName('highlighted');
+        for(i = 0;i<els.length;i++){
+            els[i].style.cssText = cssText;
+        }
+    }
+}
