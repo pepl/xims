f91416be9b833b83d444a2aecf3db398ab57c427
Michael Kröll <pepl@cpan.org>
2004-07-13 08:58:49 +0000

    fixed wrong entries in location_path columns when creating objects directly under /root using PostgreSQL

--- xims/sql/Pg/location_path.sql
+++ xims/sql/Pg/location_path.sql
@@ -8,9 +8,14 @@ DECLARE
     new_location_path TEXT;
 
 BEGIN
-    -- do we have one and only one parent_id, hope so ...
-    SELECT INTO parent_location_path location_path FROM ci_documents
-        WHERE id = parent_id_in;
+    -- catch /root, otherwise parent_location_path would be NULL and concatenation would not work
+    IF parent_id_in = 1 THEN
+        parent_location_path := '''';
+    ELSE
+        -- do we have one and only one parent_id, hope so ...
+        SELECT INTO parent_location_path location_path FROM ci_documents
+            WHERE id = parent_id_in;
+    END IF;
 
     new_location_path := parent_location_path || ''/'' || new_location_in;
 
