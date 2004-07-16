aaf3d87f499f27a3dc385fda398596b3ef3d20ed
Michael Kröll <pepl@cpan.org>
2004-07-16 19:17:22 +0000

    fixed silly typo

--- xims/www/ximsroot/scripts/default.js
+++ xims/www/ximsroot/scripts/default.js
@@ -54,7 +54,7 @@ function getParamValue( param ) {
 
 var highlighted = false;
 function stringHighlight( mystring ) {
-    if (highlighted || mysstring.length == 0 ) {
+    if (highlighted || !mystring ) {
         return;
     }
     re = /\s+/;
