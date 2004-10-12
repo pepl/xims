2ac000709fdb1b0f3f15048e88a878f5a05c4e56
Michael Kröll <pepl@cpan.org>
2004-10-12 19:30:36 +0000

    initial commit of XIMS::Iterator::Object; next -> use it in XIMS::Object::children() et al

---
+++ xims/lib/XIMS/Iterator.pm
@@ -0,0 +1,14 @@
+# Copyright (c) 2002-2004 The XIMS Project.
+# See the file "LICENSE" for information on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+# $Id$
+package XIMS::Iterator;
+
+use vars qw( $VERSION @ISA );
+use strict;
+use Array::Iterator;
+
+$VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+@ISA = ('Array::Iterator');
+
+1;
