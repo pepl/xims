1823a0bbaaec79e01712dfa599b8c52e78a05dc1
kingubu <kingubu@118f944d-ca0e-0410-82e1-b214406b748f>
2004-11-16 18:09:10 +0000

    updated NewsItem creation to allow concurrent image upload/creation

--- xims/lib/XIMS/NewsItem.pm
+++ xims/lib/XIMS/NewsItem.pm
@@ -7,8 +7,25 @@ package XIMS::NewsItem;
 use strict;
 use vars qw( $VERSION @ISA );
 use XIMS::Document;
+use XIMS::Importer::Object;
 
 $VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 @ISA = ('XIMS::Document');
+
+sub add_image {
+    XIMS::Debug( 5, "called" );
+    my $self = shift;
+    my $img_object = shift;
+
+    my $obj_importer = XIMS::Importer::Object->new( User => $img_object->User(), Parent => $img_object->parent() );
+    my $id = $obj_importer->import( $img_object );
+    if ( defined( $id ) ) {
+        $self->image_id( $id );
+    }
+    else {
+        XIMS::Debug( 3, "could not create new image object. Perhaps it already exists." );
+    }
+    return 1;
+}
 
 1;
