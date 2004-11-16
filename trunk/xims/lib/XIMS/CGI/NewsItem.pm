797de5ceb270d9e02696690c295071184b9daed2
kingubu <kingubu@118f944d-ca0e-0410-82e1-b214406b748f>
2004-11-16 18:09:10 +0000

    updated NewsItem creation to allow concurrent image upload/creation

--- trunk/xims/lib/XIMS/CGI/NewsItem.pm
+++ trunk/xims/lib/XIMS/CGI/NewsItem.pm
@@ -8,6 +8,8 @@ use strict;
 use vars qw( $VERSION @ISA );
 
 use XIMS::CGI::Document;
+use XIMS::Image;
+use Data::Dumper;
 
 # #############################################################################
 # GLOBAL SETTINGS
@@ -37,4 +39,52 @@ sub event_edit {
     return $self->SUPER::event_edit( $ctxt );
 }
 
+sub event_store {
+    my ( $self, $ctxt) = @_;
+    XIMS::Debug( 5, "called" );
+    
+    my $img_fh = $self->upload( 'imagefile' );
+     
+
+    if ( length $img_fh ) {
+        my $target_location = $self->param('imagefolder');
+        my $img_target = XIMS::Object->new( path => $target_location );
+        
+        if ( defined( $img_target )) {
+            XIMS::Debug( 5, "Creating Image object for new NewsItem" );
+            my $img_obj = XIMS::Image->new( User => $ctxt->session->user() );
+            $img_obj->parent_id( $img_target->document_id() );
+            $img_obj->location( $self->param('imagefile') );
+            my $type = $self->uploadInfo($img_fh)->{'Content-Type'};
+            my $df;
+            if ( $df = XIMS::DataFormat->new( mime_type => $type ) ) {
+                XIMS::Debug( 6, "xims mime type: ". $df->mime_type() );
+                XIMS::Debug( 6, "UA   mime type: ". $type );
+            }
+            else {
+                $df = XIMS::DataFormat->new( mime_type => 'application/octet-stream' );
+                XIMS::Debug( 6, "xims mime type: forced to 'application/octet-stream'" );
+                XIMS::Debug( 6, "UA   mime type: ". $type );
+
+            }
+            $img_obj->data_format_id( $df->id() );
+
+            XIMS::Debug(5, "reading from filehandle");
+            my ($buffer, $body);
+            while ( read($img_fh, $buffer, 1024) ) {
+                $body .= $buffer;
+            }
+
+            $img_obj->body( $body );
+            my $img_created = $ctxt->object->add_image( $img_obj );
+            XIMS::Debug( 3, "Image object import failed" ) unless $img_created;
+            
+        }
+        else {
+            XIMS::Debug( 3, "Image upload folder undefined or does not exist, the new Image object was not created" );
+        }
+    }
+
+    return $self->SUPER::event_store( $ctxt );
+}
 1;
