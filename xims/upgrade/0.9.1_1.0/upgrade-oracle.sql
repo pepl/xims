45a0c3bb123955114e1ffa1c9bffc4bdcfd72fe7
Michael Kröll <pepl@cpan.org>
2005-01-04 16:06:50 +0000

    give more space to names of users and content titles

--- xims/upgrade/0.9.1_1.0/upgrade-oracle.sql
+++ xims/upgrade/0.9.1_1.0/upgrade-oracle.sql
@@ -16,3 +16,33 @@ ALTER TABLE CI_CONTENT ADD (CONSTRAINT
 PROMPT Renaming ci_documents.status to ci_documents.document_status 
 ALTER TABLE ci_documents RENAME COLUMN status TO document_status
 /
+
+PROMPT Extending ci_content.title
+ALTER TABLE ci_content MODIFY (title VARCHAR2(400) NOT NULL)
+/
+
+PROMPT Extending lastname and firstname columns
+ALTER TABLE ci_content MODIFY (last_published_by_firstname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (created_by_firstname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (owned_by_firstname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (last_modified_by_firstname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (locked_by_firstname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (last_published_by_lastname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (created_by_lastname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (owned_by_lastname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (last_modified_by_lastname VARCHAR2(90))
+/
+ALTER TABLE ci_content MODIFY (locked_by_lastname VARCHAR2(90))
+/
+ALTER TABLE ci_users_roles MODIFY (firstname VARCHAR2(90))
+/
+ALTER TABLE ci_users_roles MODIFY (lastname VARCHAR2(90))
+/
