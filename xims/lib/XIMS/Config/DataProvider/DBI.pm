017b74a499edea29a36b763074875f9c64594e29
Michael Kröll <pepl@cpan.org>
2005-01-09 22:13:30 +0000

    DBI config reader

--- xims/lib/XIMS/Config/DataProvider/DBI.pm
+++ xims/lib/XIMS/Config/DataProvider/DBI.pm
@@ -1,64 +1,90 @@
-# Copyright (c) 2002-2004 The XIMS Project.
+# Copyright (c) 2002-2005 The XIMS Project.
 # See the file "LICENSE" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # $Id$
 package XIMS::Config::DataProvider::DBI;
 
 use strict;
-use vars qw( $VERSION );
+use vars qw($VERSION @ISA);
+$VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r; };
+@ISA = qw( XIMS::Config );
 
-$VERSION = do { my @r=(q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+use XIMS::Config;
+use XML::LibXML;
 
-sub PropertyAttributes { (
-    'content.id'                          => \'ci_content_id_seq_nval()',
-    'user.id'                             => \'ci_users_roles_id_seq_nval()',
-    'session.id'                          => \'ci_sessions_id_seq_nval()',
-    'session.last_access_timestamp'       => \'now()',
-    'document.id'                         => \'ci_documents_id_seq_nval()',
-    'bookmark.id'                         => \'ci_bookmarks_id_seq_nval()',
-    'language.id'                         => \'ci_languages_id_seq_nval()',
-    'objecttype.id'                       => \'ci_object_types_id_seq_nval()',
-    'dataformat.id'                       => \'ci_data_formats_id_seq_nval()',
-    'mimetype.id'                         => \'ci_mime_aliases_id_seq_nval()',
-    'questionnaireresult.id'              => \'ci_quest_results_id_seq_nval()',
-    'vlibauthor.id'                       => \'cilib_authors_id_seq_nval()',
-    'vlibauthormap.id'                    => \'cilib_authormap_id_seq_nval()',
-    'vlibkeyword.id'                      => \'cilib_keywords_id_seq_nval()',
-    'vlibkeywordmap.id'                   => \'cilib_keywordmap_id_seq_nval()',
-    'vlibsubject.id'                      => \'cilib_subjects_id_seq_nval()',
-    'vlibsubjectmap.id'                   => \'cilib_subjectmap_id_seq_nval()',
-    'vlibpublication.id'                  => \'cilib_publications_id_seq_nval()',
-    'vlibpublicationmap.id'               => \'cilib_publmap_id_seq_nval()',
-    'vlibmeta.id'                         => \'cilib_meta_id_seq_nval()',
-) }
+sub new {
+    my $proto = shift;
+    my $class = ref( $proto ) || $proto;
+    my $self = bless {}, $class;
+    my %args = @_;
+    my $file = ($args{filename} || (XIMS::HOME() . "/conf/ximsconfig-dbi.xml"));
 
-sub PropertyRelations { (
-    'Object' => { 'document.id' => \'ci_content.document_id' }
-) }
+    $self->process_file( $file );
 
-sub Tables { (
-            content        => 'ci_content',
-            user           => 'ci_users_roles',
-            session        => 'ci_sessions',
-            document       => 'ci_documents',
-            objecttype     => 'ci_object_types',
-            userpriv       => 'ci_roles_granted',
-            objectpriv     => 'ci_object_privs_granted',
-            objecttypepriv => 'ci_object_type_privs',
-            dataformat     => 'ci_data_formats',
-            language       => 'ci_languages',
-            mimetype       => 'ci_mime_type_aliases',
-            bookmark       => 'ci_bookmarks',
-            questionnaireresult => 'ci_questionnaire_results',
-            vlibauthor          => 'cilib_authors',
-            vlibauthormap       => 'cilib_authormap',
-            vlibkeyword         => 'cilib_keywords',
-            vlibkeywordmap      => 'cilib_keywordmap',
-            vlibsubject         => 'cilib_subjects',
-            vlibsubjectmap      => 'cilib_subjectmap',
-            vlibpublication     => 'cilib_publications',
-            vlibpublicationmap  => 'cilib_publicationmap',
-            vlibmeta            => 'cilib_meta',
-          ) }
+    return $self;
+}
+
+sub process_file {
+    my $self = shift;
+    my $file = shift;
+
+    return unless (defined $file and length $file);
+    #warn "Processing XIMS DBI config file '$file'\n";
+
+    my $parser = XML::LibXML->new();
+    my $doc;
+    eval {
+        $doc = $parser->parse_file( $file )
+    };
+    die "Could not parse file '$file': $@\n" if $@;
+
+    no strict 'refs';
+
+    my @npropattribs = $doc->findnodes( '/Config/DBI/PropertyAttributes/item' );
+    my $propattribs = {};
+    foreach my $node ( @npropattribs ) {
+        $propattribs->{$node->getAttribute('name')} = \$node->getAttribute('value');
+    }
+    # merge new properties if there already are some
+    if ( defined *{"XIMS::Config::DataProvider::DBI::PropertyAttributes"}{CODE} ) {
+        my %existing_props = XIMS::Config::DataProvider::DBI::PropertyAttributes();
+        @existing_props{keys %{$propattribs}} = values %{$propattribs};
+        $propattribs = \%existing_props;
+    }
+    # install in the symbol table
+    $self->install_methods( $propattribs, 'XIMS::Config::DataProvider::DBI', 'PropertyAttributes' );
+
+    my @ntables = $doc->findnodes( '/Config/DBI/Tables/item' );
+    my $tables = {};
+    foreach my $node ( @ntables ) {
+        $tables->{$node->getAttribute('name')} = $node->getAttribute('value');
+    }
+    # merge new properties if there already are some
+    if ( defined *{"XIMS::Config::DataProvider::DBI::Tables"}{CODE} ) {
+        my %existing_tables = XIMS::Config::DataProvider::DBI::Tables();
+        @existing_tables{keys %{$tables}} = values %{$tables};
+        $tables = \%existing_tables;
+    }
+    # install in the symbol table
+    $self->install_methods( $tables, 'XIMS::Config::DataProvider::DBI', 'Tables' );
+
+    my @nproprels = $doc->findnodes( '/Config/DBI/PropertyRelations/item' );
+    my $proprels = {};
+    foreach my $node ( @nproprels ) {
+        $proprels->{$node->getAttribute('resourcetype')} = { $node->getAttribute('property') => \$node->getAttribute('relates') };
+    }
+    # merge new properties if there already are some
+    if ( defined *{"XIMS::Config::DataProvider::DBI::PropertyRelations"}{CODE} ) {
+        my %existing_proprels = XIMS::Config::DataProvider::DBI::PropertyRelations();
+        @existing_proprels{keys %{$proprels}} = values %{$proprels};
+        $proprels = \%existing_proprels;
+    }
+    # install in the symbol table
+    $self->install_methods( $proprels, 'XIMS::Config::DataProvider::DBI', 'PropertyRelations' );
+
+    $self->process_includes( $doc );
+
+    return 1;
+}
 
 1;
