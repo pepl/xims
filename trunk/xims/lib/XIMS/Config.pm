8e6f621e018c24338726de280a7a8094dde19b36
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2005-01-09 22:10:48 +0000

    Initial commit of new config reader module

--- trunk/xims/lib/XIMS/Config.pm
+++ trunk/xims/lib/XIMS/Config.pm
@@ -1,187 +1,178 @@
-# Copyright (c) 2002-2004 The XIMS Project.
+# Copyright (c) 2002-2005 The XIMS Project.
 # See the file "LICENSE" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # $Id$
 package XIMS::Config;
 
 use strict;
-use vars qw( $VERSION );
+use vars qw($VERSION);
+$VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r; };
 
-$VERSION = do { my @r=(q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+use XIMS;
 
-use Apache;
+# Load all known DP Configs;
+use XIMS::Config::DataProvider::DBI;
+
+use XML::LibXML;
+
 ##
 #
 # SYNOPSIS
-#    XIMS::Config->new()
+#    XIMS::Config->new( [%args] )
 #
 # PARAMETER
+#    $args{filename}   (optional) :  filename of the config file
 #
 # RETURNS
-#    $config: XIMS::Config instance
+#    $config    : Instance of XIMS::Config
 #
 # DESCRIPTION
-#    Constructor; if we will have a lot config-stuff sometime, we'll move that to a 'real' config(reader)-concept
+#    Loaded by XIMS.pm; do not load it in your scripts, access it through XIMS::CONFIG()
 #
 sub new {
-    my $class = shift;
-    my $apache = Apache->request() if $ENV{MOD_PERL};
+    my $proto = shift;
+    my $class = ref( $proto ) || $proto;
+    my $self = bless {}, $class;
+    my %args = @_;
+    my $file = ($args{filename} || (XIMS::HOME() . "/conf/ximsconfig.xml") );
 
-    return bless { apache => $apache }, $class;
+    $self->process_file( $file );
+
+    # try to load DataProvider specific config
+    my $dpclass = "XIMS::Config::DataProvider::" . $self->DBMS();
+    my $dpconfig = $dpclass->new();
+
+    $self->{dpconfig} = $dpconfig;
+
+    return $self;
 }
 
+sub general { shift->{general} }
+sub dpconfig { shift->{dpconfig} }
 
+sub process_file {
+    my $self = shift;
+    my $file = shift;
 
-# ****************************************
-#
-# Debuglevel
-#
-# values from 0..6, where 6 is the most verbose.
-#
-# see
-# http://xims.uibk.ac.at/xims-doku/developers/developer-guide.dkb#id2832938
-# for the "Debuglevel Reference"
-#
-# ****************************************
-sub DebugLevel()                { '6' }
+    unless ( defined $file and -f $file and -r $file ) {
+        die "Could not access '$file'.\n";
+    }
+    #warn "Processing XIMS config file '$file'\n";
 
-# ****************************************
-#
-# paths and files...
-#
-# ****************************************
+    my $parser = XML::LibXML->new();
+    my $doc;
+    eval {
+        $doc = $parser->parse_file( $file )
+    };
+    die "Could not parse file '$file': $@\n" if $@;
 
-# apache document_root
-sub ApacheDocumentRoot()          { '' }            # Absolute path to the Apache document root
+    my @ngeneral = $doc->findnodes( '/Config/General/*' );
+    my $general;
+    if ( scalar @ngeneral ) {
+        $general = $self->get_values( @ngeneral );
+        $self->install_methods( $general );
+        $self->{general} = $general;
+    }
 
-sub XIMSRoot()                    { 'ximsroot' }    # Path to ximsroot,relative to ApacheDocumentRoot()
-                                                    # Do not change unless you know what you are doing!
+    no strict 'refs';
 
-sub PublicRoot()                  { 'ximspubroot'}  # Path to the export directory relative to ApacheDocumentRoot()
-                                                    # Must be in sync with the corresponding value in config.xsl
-                                                    # Do not change unless you know what you are doing!
+    my @nrt = $doc->findnodes( '/Config/Names/ResourceTypes' );
+    my $resource_types = $self->get_values( @nrt );
+    # merge new properties if there already are some
+    if ( defined *{"XIMS::Config::Names::ResourceTypes"}{CODE} ) {
+        my @existing_rst = XIMS::Config::Names::ResourceTypes();
+        unshift (@{$resource_types->{ResourceTypes}}, @existing_rst) if scalar @existing_rst;
+    }
+    # install in the symbol table
+    $self->install_methods( $resource_types, 'XIMS::Config::Names' );
 
-sub goxims()                      { 'goxims' }      # Name of the main XIMS PerlHandler module
-                                                    # Must be in sync with the corresponding value in config.xsl
-                                                    # Do not change unless you know what you are doing!
+    my @nproperties = $doc->findnodes( '/Config/Names/Properties/*' );
+    my $properties = $self->get_values( @nproperties );
+    # merge new properties if there already are some
+    if ( defined *{"XIMS::Config::Names::Properties"}{CODE} ) {
+        my %existing_props = XIMS::Config::Names::Properties();
+        @existing_props{keys %{$properties}} = values %{$properties};
+        $properties = \%existing_props;
+    }
+    # install in the symbol table
+    $self->install_methods( $properties, 'XIMS::Config::Names', 'Properties' );
 
-sub ContentInterface()            { 'content' }     # Name of the content interface
-                                                    # Must be in sync with the corresponding value in config.xsl
-                                                    # Do not change unless you know what you are doing!
+    $self->process_includes( $doc );
 
-sub DefaultSkin()                 { 'default' }     # Name of the skin used per default. Relative path under XIMSRoot()/skins/
-                                                    # Do not change unless you know what you are doing!
+    return 1;
+}
 
-sub ResolveRelToSiteRoots()       { '1' }           # <'0'|'1'>
-                                                    # Set this to '1' if you want to symlink OU's directly under
-                                                    # ApacheDocumentRoot() or use virtual hosts
-                                                    # Do not change unless you know what you are doing!
+sub get_values {
+    my $self = shift;
+    my @nodes = @_;
+    my %rv;
+    foreach my $node ( @nodes ) {
+        my @items = $node->findnodes( 'item' );
+        if ( scalar @items ) {
+            my @values = map { $_->textContent() } @items;
+            $rv{$node->localname()} = \@values;
+        }
+        else {
+            $rv{$node->localname()} = $node->textContent();
+        }
+    }
+    return \%rv;
+}
 
-sub SearchResultRowLimit()        { '20' }          # Number of found objects displayed per page at event_search
-                                                    # Must be in sync with the corresponding value in config.xsl
+sub install_methods {
+    my $self = shift;
+    my $methods = shift;
+    my $package = ( shift || __PACKAGE__ );
+    my $return_hash = shift;
 
+    $package .=  "::";
+    #warn "package: $package\n";
 
-# some names for "special" files
-sub AutoIndexFilename()           { '_ottereendex.html' }      # Filename of the auto-generated container indices.
-                                                               # If you change that, you have to update the DirectoryIndex directive in ximshttpd.conf
-sub DocFSExportStylesheet()       { 'document_export.xsl' }
-sub AutoindexExportStylesheet()   { 'export_auto_index.xsl' }
+    no strict 'refs';
+    no warnings 'redefine';
+    my $code;
+    if ( defined $return_hash ) {
+        $code = sub { return %{$methods} };
+        *{"$package$return_hash"} = $code;
+    }
+    else {
+        my ($name, $value);
+        while (($name, $value) = each %{$methods}) {
+            if ( ref $value eq 'ARRAY' ) {
+                $code = eval "sub { return qw{ @{$value} } }";
+            }
+            else {
+                $code = eval "sub { return q{$value} }";
+            }
+            *{"$package$name"} = $code;
+        }
+    }
+}
 
-# fallback in the absence of defaultbookmark
-sub DefaultStartingPath()         { '/xims' }   # If users have no defaultbookmark set, they will be redirected to /$goxims/$contentinterface/$defaultstartingpath
-                                                # Value must be the path to an existing XIMS content object
+sub process_includes {
+    my $self = shift;
+    my $doc = shift;
 
-sub WYSIWYGEditor()               { '' }        # Currently, 'htmlarea' and 'wepro' are implemented.
-                                                # If you configure WYSIWYGEditor(), you have to install the corresponding editor files to XIMSRoot(). For example, download HTMLArea 3 CVS from
-                                                # http://sourceforge.net/projects/itools-htmlarea/ and extract it to XIMSRoot() so that htmlarea.js is available at $XimsRoot()/htmlarea/htmlarea.js.
-                                                # The files for 'wepro' are available at http://www.ektron.com/ewebeditpro.cfm?doc_id=1989
-                                                # Unpack them to XIMSRoot()/ewebedit/
-                                                #
-                                                # WYSIWYGEditor() should be considered as a system wide default an fall back value. Users should be able to set their preference via the /user/ interface (to be developed)
+    my @includes = map { $_->textContent() } $doc->findnodes( '/Config/Include' );
+    foreach my $path ( @includes ) {
+        $path = XIMS::HOME() . "/conf/" . $path;
+        if ( -d $path and -x $path ) {
+            opendir(DIR, $path) || die "Could not open directory '$path': $!\n";
+            my @files = grep { /\.xml$/ && -f "$path/$_" && -r "$path/$_" } readdir(DIR);
+            closedir DIR;
+            foreach my $f ( @files ) {
+                $self->process_file( "$path/$f" );
+            }
+        }
+        elsif ( -f $path and -r $path ) {
+            $self->process_file( $path );
+        }
+        else {
+            die "Could not access included config '$path': $!\n";
+        }
+    }
 
-sub XMLEditor()                   { '' }        # Currently 'bxe' version 0.9.5 is implemented
-                                                # You can download it from http://www.bitfluxeditor.org.
-                                                # Unpack the files into XIMSRoot()/bxe/
-
-
-# path and options for tidy
-sub TidyPath()                    {'/usr/local/bin/tidy'};                                              # path to tidy binary
-sub TidyOptions()                 {' -config /usr/local/xims/conf/ximstidy.conf -quiet -f /dev/null'};  # options for tidy
-
-# ****************************************
-#
-# authentification
-#
-# ****************************************
-
-sub AuthStyle()                   { 'XIMS::Auth::Password' }   # Name or comma-separated list of names of the authentification module(s)
-                                                               # Modules in a list will be tried consecutively.
-sub AuthServer()                  { '' }                       # FQDN of the Server used for LDAP or IMAP authentification
-
-
-
-# ****************************************
-#
-# "special" userids
-#
-# ****************************************
-
-sub PublicUserID()                { '6' }                  # Default value comes from default_data.sql
-                                                           # Do not change unless you know what you are doing!
-sub AdminRoleID()                 { '4' }                  # Default value comes from default_data.sql
-                                                           # Do not change unless you know what you are doing!
-
-
-# ****************************************
-#
-# db-stuff
-#
-# ****************************************
-
-sub DBMS()                        { 'DBI' }
-sub DBUser()                      { '' }
-sub DBName()                      { '' }
-sub DBPassword()                  { '' }
-
-sub DBdsn()                       { '' }                # DBI dsn string
-sub DBDOpt()                      { '' }                # RDBMS specific DBD connect options
-sub DBSessionOpt()                { '' }                # RDBMS specific DBD session options
-
-# Example settings for PostgreSQL
-#sub DBUser()                      { 'ximsrun' } # Default value from defaultdata.sql
-#sub DBName()                      { 'xims' }    # Default value from defaultdata.sql
-#sub DBPassword()                  { 'ximsrun' } # Default value from defaultdata.sql
-#sub DBdsn()                       { 'dbi:Pg:dbname=xims' }
-#sub DBSessionOpt()                { 'SET DateStyle TO German;SET Client_Encoding TO UNICODE;' }
-#sub DBDOpt()                      { 'FetchHashKeyName=NAME_uc;' }
-
-# Examples settings for Oracle
-#sub DBdsn()                       { 'dbi:Oracle:test' }
-#sub DBSessionOpt()                { 'ALTER SESSION SET NLS_DATE_FORMAT=\'DD.MM.YYYY HH24:MI:SS\';' }
-#sub DBDOpt()                      { 'LongReadLen=10485760;LongTruncOk=1'};
-
-sub DBEncoding()                  { '' }                   # Set this to your dabase encoding if the database is not encoded in UTF-8
-sub QBDriver()                    { '' }                   # Used for special DBI specific QueryBuilder Driver.
-                                                           # 'InterMedia' for example will load XIMS::QueryBuilder::OracleInterMedia
-                                                           # for Oracle.
-
-
-# ****************************************
-#
-# Language settings
-#
-# ****************************************
-
-sub DefaultLanguage()             { 'en-us' }                # (default)language set on object creation
-                                                             # while objects with (even multiple) non-default
-                                                             # language content(s) are in the datastructures,
-                                                             # there's no usable implemetation for it UI-wise yet;
-sub UILanguages()                 { ("de-at", "en-us") }     # Array of language-tags XIMS has UI-Stylesheets for.
-                                                             # maps 1:1 to the names of the subdir in
-                                                             # /ximsroot/skins/default/stylesheets
-                                                             # i.e: /ximsroot/skins/default/stylesheets/en-us/
-                                                             #                                          ^^^^^
-sub UIFallbackLang()              { 'en-us' }                # XIMS will fall back to this, if it cannot provide
-                                                             # stylesheets for the language requested by the UserAgent;
+    return 1;
+}
 
 1;
