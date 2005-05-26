a3a4642ae3db7db69a4df3e070036169e66272d2
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2005-05-26 09:22:15 +0000

    XIMS::Entities does not export decode() any longer

--- trunk/xims/lib/XIMS/Document.pm
+++ trunk/xims/lib/XIMS/Document.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2002-2004 The XIMS Project.
+# Copyright (c) 2002-2005 The XIMS Project.
 # See the file "LICENSE" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # $Id$
@@ -9,7 +9,7 @@ use vars qw( $VERSION @ISA );
 use XIMS;
 use XIMS::Object;
 use XIMS::DataFormat;
-use XIMS::Entities qw(decode);
+use XIMS::Entities;
 
 $VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 @ISA = ('XIMS::Object');
