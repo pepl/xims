b097b019569eb43df49fcfa3bd744ea9587b5a87
Michael Kröll <pepl@cpan.org>
2005-08-11 15:16:13 +0000

    Updated year in copyright line to -2005

--- xims/lib/XIMS/Questionnaire.pm
+++ xims/lib/XIMS/Questionnaire.pm
@@ -1,4 +1,4 @@
-## copyrightino (c) 2002-2004 The XIMS Project.
+## copyrightino (c) 2002-2005 The XIMS Project.
 # See the file "LICENSE" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # $Id$
