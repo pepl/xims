aa6bd84167ff89c90d2a5b713be97a01f1e6eb51
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2005-10-11 08:47:17 +0000

    Use B-Tree- instead of Hash-Indexes for Pg

---
+++ trunk/xims/upgrade/1.0_1.1/upgrade-pg.sql
@@ -0,0 +1,13 @@
+-- From: http://www.postgresql.org/docs/7.4/interactive/indexes-types.html
+-- Note:  Testing has shown PostgreSQL's hash indexes to perform no better than B-tree indexes,
+-- and the index size and build time for hash indexes is much worse. For these reasons,
+-- hash index use is presently discouraged.
+\echo Recreating HASH indexes as B-TREE indexes
+DROP index ci_content_doc_id_idx;
+CREATE INDEX ci_content_doc_id_idx ON ci_content ( document_id );
+
+DROP index ci_roles_granted_grantee_idx;
+CREATE INDEX ci_roles_granted_grantee_idx ON ci_roles_granted ( grantee_id );
+
+DROP index ci_obj_type_privs_grantee_idx;
+CREATE INDEX ci_obj_type_privs_grantee_idx ON ci_object_type_privs ( grantee_id );
