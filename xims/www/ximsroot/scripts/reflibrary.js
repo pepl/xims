c19f12bf4e63b2d40eb934d362411c0de6efd8ca
Michael Kröll <pepl@cpan.org>
2005-11-27 23:02:26 +0000

    Initial import of Reference Library script

---
+++ xims/www/ximsroot/scripts/reflibrary.js
@@ -0,0 +1,10 @@
+/*
+# Copyright (c) 2002-2005 The XIMS Project.
+# See the file "LICENSE" for information on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+# $Id$
+*/
+
+function getRefTypeDescription( reftype_id ) {
+    return document.getElementById( 'reftype' + reftype_id ).innerHTML;
+}
