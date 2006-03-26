c1b748d44dd17e6ad766af287c05336198045642
Michael Kröll <pepl@cpan.org>
2006-03-26 21:39:26 +0000

    * Updated LICENSE sentence

--- xims/upgrade/0.5_0.6/upgrade_ora_eval.sql
+++ xims/upgrade/0.5_0.6/upgrade_ora_eval.sql
@@ -1,6 +1,6 @@
 -- Copyright (c) 2002-2006 The XIMS Project.
--- See the file "LICENSE" for information on usage and redistribution
--- of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+-- See the file "LICENSE" for information and conditions for use, reproduction,
+-- and distribution of this work, and for a DISCLAIMER OF ALL WARRANTIES.
 -- $Id$
 
 -- Add two new object types: Questionnaire and TAN_List
