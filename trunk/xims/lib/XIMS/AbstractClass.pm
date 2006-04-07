5b776b1013b85cdfa2dbf21edf0414dfa95871c2
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2006-04-07 23:10:00 +0000

    cleaned up $Id$ svn keyword line

--- trunk/xims/lib/XIMS/AbstractClass.pm
+++ trunk/xims/lib/XIMS/AbstractClass.pm
@@ -1,6 +1,7 @@
 # Copyright (c) 2002-2006 The XIMS Project.
 # See the file "LICENSE" for information and conditions for use, reproduction,
 # and distribution of this work, and for a DISCLAIMER OF ALL WARRANTIES.
+# $Id$
 package XIMS::AbstractClass;
 
 use strict;
