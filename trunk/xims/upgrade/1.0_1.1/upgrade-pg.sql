458f62fc0ca17596709f1e7984a68d2a2d70fe01
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2006-04-23 20:38:50 +0000

    * Added WebDAV object type privilege management

--- trunk/xims/upgrade/1.0_1.1/upgrade-pg.sql
+++ trunk/xims/upgrade/1.0_1.1/upgrade-pg.sql
@@ -13,12 +13,12 @@ DROP index ci_obj_type_privs_grantee_idx;
 CREATE INDEX ci_obj_type_privs_grantee_idx ON ci_object_type_privs ( grantee_id );
 
 \echo Adding new attributes to CILIB_META
-ALTER TABLE cilib_meta ADD COLUMN coverage            VARCHAR(256)
-ALTER TABLE cilib_meta ADD COLUMN publisher           VARCHAR(256)
-ALTER TABLE cilib_meta ADD COLUMN audience            VARCHAR(256)
-ALTER TABLE cilib_meta ADD COLUMN dc_date             TIMESTAMP(0) WITHOUT TIME ZONE
-ALTER TABLE cilib_meta ADD COLUMN date_from_timestamp TIMESTAMP(0) WITHOUT TIME ZONE
-ALTER TABLE cilib_meta ADD COLUMN date_to_timestamp   TIMESTAMP(0) WITHOUT TIME ZONE
+ALTER TABLE cilib_meta ADD COLUMN coverage            VARCHAR(256);
+ALTER TABLE cilib_meta ADD COLUMN publisher           VARCHAR(256);
+ALTER TABLE cilib_meta ADD COLUMN audience            VARCHAR(256);
+ALTER TABLE cilib_meta ADD COLUMN dc_date             TIMESTAMP(0) WITHOUT TIME ZONE;
+ALTER TABLE cilib_meta ADD COLUMN date_from_timestamp TIMESTAMP(0) WITHOUT TIME ZONE;
+ALTER TABLE cilib_meta ADD COLUMN date_to_timestamp   TIMESTAMP(0) WITHOUT TIME ZONE;
 
 \echo adding new VLibrary ObjectTypes
 INSERT INTO ci_object_types ( id, name, is_fs_container, is_xims_data, redir_to_self, publish_gopublic, parent_id )
@@ -34,3 +34,40 @@ INSERT INTO ci_object_types ( id, name, is_fs_container, is_xims_data, redir_to_
 
 \echo Adding SimpleDB objects
 \i ../../sql/Pg/create_simpledb.sql
+
+\echo Adding new attributes to CI_OBJECT_TYPES
+ALTER TABLE ci_object_types ADD COLUMN is_davgetable    SMALLINT DEFAULT 0;
+ALTER TABLE ci_object_types ADD COLUMN davprivval       NUMERIC(32,0);
+
+\echo Adding new attribute to CI_USERS_ROLES
+ALTER TABLE ci_users_roles ADD COLUMN dav_otprivs_mask    NUMERIC(32,0);
+
+\echo Setting is_ci_object_types.davgetable
+UPDATE ci_object_types SET is_davgetable = 0;
+UPDATE ci_object_types SET is_davgetable = 1 WHERE id IN (SELECT id from ci_object_types WHERE name IN ('Annotation', 'AxPointPresentation', 'CSS', 'DepartmentRoot', 'DocBookXML', 'Document', 'File', 'Folder', 'Image', 'JavaScript', 'NewsItem', 'Portal', 'Portlet', 'Questionnaire', 'SQLReport', 'SiteRoot', 'Text', 'XML', 'XSLStylesheet', 'XSPScript', 'sDocBookXML'));
+
+\echo Setting ci_object_types.davprivval
+UPDATE ci_object_types SET davprivval = 0;
+-- Container and Binary types
+UPDATE ci_object_types SET davprivval = 1 WHERE name IN ('DepartmentRoot', 'File', 'Folder', 'Image', 'SiteRoot');
+-- Text based types
+UPDATE ci_object_types SET davprivval = 2 WHERE name IN ('CSS', 'JavaScript', 'Text');
+-- XML types
+UPDATE ci_object_types SET davprivval = 4 WHERE name IN ('AxPointPresentation', 'DocBookXML', 'XML', 'XSLStylesheet', 'XSPScript', 'sDocBookXML');
+
+UPDATE ci_object_types SET davprivval = 8 WHERE name = 'Document';
+UPDATE ci_object_types SET davprivval = 16 WHERE name = 'NewsItem';
+UPDATE ci_object_types SET davprivval = 32 WHERE name = 'Questionnaire';
+UPDATE ci_object_types SET davprivval = 64 WHERE name = 'SQLReport';
+UPDATE ci_object_types SET davprivval = 128 WHERE name = 'Portlet';
+
+\echo Setting ci_users_roles.dav_otprivs_mask;
+-- Container, Binary, Text
+UPDATE ci_users_roles SET dav_otprivs_mask = 3;
+-- All types (0xFFFFFFFF bit bitmask) for admin
+UPDATE ci_users_roles SET dav_otprivs_mask = 4294967295 WHERE admin = 1;
+
+-- Clean that up
+UPDATE ci_users_roles SET system_privs_mask = 4294967295 WHERE admin = 1;
+
+
