29a0bd8f3131d82da522b1c441e7f3c8c662fc8d
Michael Kröll <pepl@cpan.org>
2006-05-01 12:03:17 +0000

    pass right parameters

--- xims-admin/release_builder/build-release.sh
+++ xims-admin/release_builder/build-release.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ -z "$2" ]; then
-    echo "Usage: $0 developer-name release-number"
+if [ -z "$1" ]; then
+    echo "Usage: $0 release-number"
     exit 1
 fi
 
-CVS_RSH=ssh cvs -d:ext:$1@cvs.sf.net:/cvsroot/xims export -D tomorrow -d xims-$2 xims
-CVS_RSH=ssh cvs -d:ext:$1@cvs.sf.net:/cvsroot/xims export -D tomorrow -d xims-$2-contrib xims-contrib
-mv xims-$2-contrib/htmlarea3rc1 xims-$2/www/ximsroot/htmlarea
-mv xims-$2-contrib/jscalendar-1.0 xims-$2/www/ximsroot/
-cd xims-$2
+svn export https://svn.sourceforge.net/svnroot/xims/trunk/xims xims-$1
+svn export https://svn.sourceforge.net/svnroot/xims/trunk/xims-contrib xims-$1-contrib
+mv xims-$1-contrib/htmlarea3rc1 xims-$1/www/ximsroot/htmlarea
+mv xims-$1-contrib/jscalendar-1.0 xims-$1/www/ximsroot/
+cd xims-$1
 find -type f -exec chmod 644 {} \;
 find -iname "*.pl" -exec chmod 755 {} \;
 find tools -name "*.pl" -exec chown 0 {} \;
@@ -20,16 +20,16 @@ chmod 440 conf/ximsconfig.xml
 chmod 6775 www/ximspubroot
 
 cd ..
-mkdir xims-$2/documentation/api
-find . -regex ".*xims-$2.lib.*\.pm" -exec sh _build-apidoc.sh {} xims-$2/documentation/api \;
-find . -regex ".*xims-$2.documentation.*\.sdbk" -exec sh _build-doc.sh {} \;
+mkdir xims-$1/documentation/api
+find . -regex ".*xims-$1.lib.*\.pm" -exec sh _build-apidoc.sh {} xims-$1/documentation/api \;
+find . -regex ".*xims-$1.documentation.*\.sdbk" -exec sh _build-doc.sh {} \;
 
-find xims-$2 -type d -name "originals" | xargs rm -rf
+find xims-$1 -type d -name "originals" | xargs rm -rf
 
-cp README.html xims-$2/documentation/api
+cp README.html xims-$1/documentation/api
 
-GZIP=-9 tar zcvf xims-$2.tar.gz xims-$2
-#rm -rf xims-$2
-#rm -rf xims-$2-contrib
+GZIP=-9 tar zcvf xims-$1.tar.gz xims-$1
+#rm -rf xims-$1
+#rm -rf xims-$1-contrib
 
-md5sum xims-$2.tar.gz > xims-$2.tar.gz.md5sum
+md5sum xims-$1.tar.gz > xims-$1.tar.gz.md5sum
