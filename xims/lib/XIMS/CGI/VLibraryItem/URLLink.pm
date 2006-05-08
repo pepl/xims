6e4e94390baebfc4366091a3e978c5ed1758d993
jokar <jokar@118f944d-ca0e-0410-82e1-b214406b748f>
2006-05-08 13:13:36 +0000

    Fixed mapping and creation of subjects and keywords

--- xims/lib/XIMS/CGI/VLibraryItem/URLLink.pm
+++ xims/lib/XIMS/CGI/VLibraryItem/URLLink.pm
@@ -44,15 +44,20 @@ sub event_store {
         $self->SUPER::event_store( $ctxt );
         $meta = XIMS::VLibMeta->new( document_id => $object->document_id());
     }
-    $object->vlesubjects( subjects_from_param( $self->param('subject') ) );
-    $object->vlekeywords( keywords_from_param( $self->param('keywords') ) );
-    
-    # Store metadata (publisher, chronicle dates)
 
-    $meta->data( publisher => $self->param('publisher') );
+    # store subjects and keywords
+    my $vlsubject = $self->param('vlsubject');
+    my $vlkeyword = $self->param('vlkeyword');
+    $self->_create_mapping_from_name( $ctxt->object(), 'Subject', $vlsubject ) if $vlsubject;
+    $self->_create_mapping_from_name( $ctxt->object(), 'Keyword', $vlkeyword ) if $vlkeyword;
+
+    # Store metadata (publisher, chronicle dates, etc)
+    map { $meta->data( $_=>$self->param($_) ) if defined $self->param($_) }
+          qw(publisher subtitle legalnotice bibliosource mediatype coverage audience);
+
     my $date_from = $self->param('chronicle_from_date');
     if ($date_from) {
-        if ( isvaliddate( $date_from ) ) {
+        if ( $self->_isvaliddate( $date_from ) ) {
             $meta->data( date_from_timestamp => $date_from );
         }
         else {
@@ -61,7 +66,7 @@ sub event_store {
     }
     my $date_to = $self->param('chronicle_to_date');
     if ($date_to) {
-        if ( isvaliddate( $date_to ) ) {
+        if ( $self->_isvaliddate( $date_to ) ) {
             $meta->data( date_to_timestamp => $date_to );
         }
         else {
@@ -78,76 +83,3 @@ sub event_store {
         return 1;
     }
 }
-
-sub keywords_from_param {
-    XIMS::Debug( 5, "called" );
-
-    my $keywords = shift;
-    my @keyword_list = split /,/, $keywords;
-    my @vl_keywords;
-
-    return () unless $keywords;
-
-    foreach my $keyword ( @keyword_list ) {
-        next until $keyword;
-        my $vlibkeyword = XIMS::VLibKeyword->new( name => $keyword );
-        if ( not ( defined $vlibkeyword and $vlibkeyword->id ) ) {
-            $vlibkeyword = XIMS::VLibKeyword->new();
-            $vlibkeyword->name( $keyword );
-            if ( not $vlibkeyword->create() ) {
-                XIMS::Debug( 3, "could not create VLibKeyword $keyword" );
-                next;
-            } else  {
-                XIMS::Debug( 6, "created keyword $keyword" );
-            }
-        }
-        push(@vl_keywords, $vlibkeyword);
-    }
-    
-    return @vl_keywords;
-}
-
-sub subjects_from_param {
-    XIMS::Debug( 5, "called" );
-
-    my $subjects = shift;
-    my @subject_list = split /,/, $subjects;
-    my @vl_subjects;
-
-    return () unless $subjects;
-
-    foreach my $subject ( @subject_list ) {
-        next until $subject;
-        my $vlibsubject = XIMS::VLibSubject->new( name => $subject );
-        if ( not (defined $vlibsubject and $vlibsubject->id) ) {
-            $vlibsubject = XIMS::VLibSubject->new();
-            $vlibsubject->name( $subject );
-            if ( not $vlibsubject->create() ) {
-                XIMS::Debug( 3, "could not create VLibSubject $subject" );
-                next;
-            }
-        }
-        push(@vl_subjects, $vlibsubject);
-    }
-
-    return @vl_subjects;
-}
-
-sub isvaliddate {
-  my $input = shift;
-  if ($input =~ m!^((?:17|18|19|20)\d\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$!) {
-    # At this point, $1 holds the year, $2 the month and $3 the day of the date entered
-    if ($3 == 31 and ($2 == 4 or $2 == 6 or $2 == 9 or $2 == 11)) {
-      return 0; # 31st of a month with 30 days
-    } elsif ($3 >= 30 and $2 == 2) {
-      return 0; # February 30th or 31st
-    } elsif (($2 == 2) and ($3 == 29) and not ((($1 % 4) == 0) and ( (($1 % 100) != 0 ) or ( ($1 % 400) == 0 )))) {
-      return 0; # February 29th outside a leap year
-    } else {
-      return 1; # Valid date
-    }
-  } else {
-    return 0; # Not a date
-  }
-}
-1;
