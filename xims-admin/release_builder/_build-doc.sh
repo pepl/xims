7b12f88663a33b7ec0d3667ae8d7aea1ecd85877
Michael Kröll <pepl@cpan.org>
2006-07-08 17:14:14 +0000

    Tagged release 1.2

--- xims-admin/release_builder/_build-doc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#echo 'xsltproc --param shade.verbatim "1" --stringparam html.stylesheet http://xims.info/stylesheets/docbook.css -o `dirname $1`/`basename $1 sdbk`html docbook/xhtml/docbook.xsl $1' > d.sh
-NEWNAME=`echo $1 | sed -e s/sdbk/html/`
-xsltproc --stringparam html.stylesheet http://xims.info/stylesheets/docbook.css -o $NEWNAME docbook/xhtml/docbook.xsl $1
-perl -pi -e 's#.sdbk#.html#g' $NEWNAME
-rm -f $1
-#find . -regex ".*$1.documentation.*\.sdbk" -exec rm -f {} \;
