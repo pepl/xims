4f066ae9ed92133c219275b7d461b88ab2ab7ac4
jokar <jokar@118f944d-ca0e-0410-82e1-b214406b748f>
2006-08-08 11:05:52 +0000

    Add URL-check for XIMS::URLLink

--- trunk/xims/lib/XIMS/URLLink.pm
+++ trunk/xims/lib/XIMS/URLLink.pm
@@ -7,6 +7,7 @@ package XIMS::URLLink;
 use strict;
 use base qw( XIMS::Object );
 use XIMS::DataFormat;
+use LWP::UserAgent;
 
 our ($VERSION) = ( q$Revision$ =~ /\s+(\d+)\s*$/ );
 
@@ -34,6 +35,42 @@ sub new {
     }
 
     return $class->SUPER::new( %args );
+}
+
+##
+#
+# SYNOPSIS
+#    XIMS::URLLink->check( [$url] )
+#
+# PARAMETER
+#    $url: optional URL to check. If missing the object->location is checked
+#
+# RETURNS
+#    $status: 1 if HTTP-Response code begins with 2 or 3
+#             0 if HTTP-Response code begins with 4 or 5
+#
+# DESCRIPTION
+#    Checks the HTTP-Status of the URL-link and stores 
+#    the result in the database 
+#       * attribute 'HTTP_Status': status line (code and message)
+#       * attribute 'last_checked': date of last check
+#
+sub check {
+    my $self = shift;
+    my $url = shift;
+    
+    $url = $self->location if (not $url);
+    my $ua = LWP::UserAgent->new;
+    $ua->agent('XIMS URL-Check');
+    my $req = HTTP::Request->new(GET => $url);
+    my $res = $ua->request( $req );
+    #if object is already created store status as attributes
+    if ($self->document_id()) {
+        $self->attribute( http_status => $res->status_line );
+        $self->attribute( last_checked => $self->data_provider->db_now() );
+    }
+    return 1 if ($res->code =~ (/^[23]/));
+    return 0;
 }
 
 1;
