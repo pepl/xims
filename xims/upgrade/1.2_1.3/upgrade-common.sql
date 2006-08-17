cb7c4163d25b50ab9cf2c6cc5f8bb11707ff15c2
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2006-08-17 14:35:45 +0000

    + sequence independence

--- xims/upgrade/1.2_1.3/upgrade-common.sql
+++ xims/upgrade/1.2_1.3/upgrade-common.sql
@@ -2,38 +2,38 @@ INSERT INTO cireflib_reference_properties ( id, name, description, position )
        VALUES ( cireflib_refprop_id_seq_nval(), 'quality_criterion', '"Quality criterion": One of "peer reviewed", "not peer reviewed", "invited", "popular scientific"', 2400 );
 
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 1 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 1 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 2 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 2 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 3 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 3 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 4 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 4 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 5 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 5 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 6 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 6 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 7 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 7 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 8 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 8 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 9 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 9 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 10 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 10 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 11 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 11 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 12 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 12 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 13 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 13 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 14 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 14 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 15 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 15 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 16 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 16 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 17 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 17 );
 INSERT INTO cireflib_ref_type_propertymap ( id, property_id, reference_type_id )
-       VALUES ( cireflib_rtpm_id_seq_nval(), 45, 18 );
+       VALUES ( cireflib_rtpm_id_seq_nval(), (select id from cireflib_reference_properties where name = 'quality_criterion'), 18 );
