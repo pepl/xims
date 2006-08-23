003bf482d7fc6ccc853cbd7dcede40be92af42f6
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2006-08-23 10:04:34 +0000

    + new class XIMS::SAX::Generator::Exporter::VLibraryItem derived from   XIMS::SAX::Generator::Exporter; + used it in XIMS::Exporter::VLibraryItem(::Document); (fixes <links/>) + insert the expensive Authors-, Subjects-, and Publicationslists only   on event_edit.

---
+++ xims/lib/XIMS/SAX/Generator/Exporter/VLibraryItem.pm
@@ -0,0 +1,44 @@
+# Copyright (c) 2002-2006 The XIMS Project.
+# See the file "LICENSE" for information and conditions for use, reproduction,
+# and distribution of this work, and for a DISCLAIMER OF ALL WARRANTIES.
+# $Id: VLibraryItem.pm 1549 2006-08-04 11:55:41Z jokar $
+package XIMS::SAX::Generator::Exporter::VLibraryItem;
+
+use strict;
+use base qw(XIMS::SAX::Generator::Exporter XIMS::SAX::Generator::VLibraryItem);
+
+our ($VERSION) = ( q$Revision: 1549 $ =~ /\s+(\d+)\s*$/ );
+
+##
+#
+# SYNOPSIS
+#    $generator->prepare( $ctxt );
+#
+# PARAMETER
+#    $ctxt : the appcontext object
+#
+# RETURNS
+#    $doc_data : hash ref to be given to be mangled by XML::Generator::PerlData
+#
+# DESCRIPTION
+#
+#
+sub prepare {
+    XIMS::Debug( 5, "called" );
+    my $self = shift;
+    my $ctxt = shift;
+
+    my $doc_data = $self->SUPER::prepare( $ctxt );
+    
+    use Data::Dumper;
+    warn "XIMS::SAX::Generator::Exporter::VLibraryItem", Dumper($doc_data);
+    
+    # The body won't be parsed by XML::Filter::CharacterChunk with the XML decl. Might be slightly hacky but its effective ;-)
+    $doc_data->{context}->{object}->{body} =~ s/^<\?xml[^>]+>//;
+
+    $self->_insert_vle_common($ctxt, $doc_data);
+    
+    return $doc_data;
+}
+
+1;
--- xims/lib/XIMS/SAX/Generator/VLibraryItem.pm
+++ xims/lib/XIMS/SAX/Generator/VLibraryItem.pm
@@ -9,6 +9,28 @@ use base qw(XIMS::SAX::Generator::Content);
 
 our ($VERSION) = ( q$Revision$ =~ /\s+(\d+)\s*$/ );
 
+# code shared between XIMS::SAX::Generator::VLibraryItem 
+# and XIMS::SAX::Generator::Exporter::VLibraryItem  
+sub _insert_vle_common {
+    my ($self, $ctxt, $doc_data) = splice(@_, 0, 3);
+
+    my @authors = $ctxt->object->vleauthors();
+    $doc_data->{context}->{object}->{authorgroup} = { author => \@authors } if ( !($authors[0] eq undef ) );
+
+    my @keywords = $ctxt->object->vlekeywords();
+    $doc_data->{context}->{object}->{keywordset} = { keyword => \@keywords } if ( !($keywords[0] eq undef ) );
+
+    my @subjects = $ctxt->object->vlesubjects();
+    $doc_data->{context}->{object}->{subjectset} = { subject => \@subjects } if ( !($subjects[0] eq undef ) );
+
+    my @publications = $ctxt->object->vlepublications();
+    $doc_data->{context}->{object}->{publicationset} = { publication => \@publications };
+
+    my $meta = $ctxt->object->vlemeta();
+    $doc_data->{context}->{object}->{meta} = $meta;
+
+}
+
 ##
 #
 # SYNOPSIS
@@ -27,38 +49,27 @@ sub prepare {
     XIMS::Debug( 5, "called" );
     my $self = shift;
     my $ctxt = shift;
-
+  
     my $doc_data = $self->SUPER::prepare( $ctxt );
-
+  
     # The body won't be parsed by XML::Filter::CharacterChunk with the XML decl. Might be slightly hacky but its effective ;-)
     $doc_data->{context}->{object}->{body} =~ s/^<\?xml[^>]+>//;
-
-    my @authors = $ctxt->object->vleauthors();
-    $doc_data->{context}->{object}->{authorgroup} = { author => \@authors } if ( !($authors[0] eq undef ) );
-
-    my @keywords = $ctxt->object->vlekeywords();
-    $doc_data->{context}->{object}->{keywordset} = { keyword => \@keywords } if ( !($keywords[0] eq undef ) );
-
-    my @subjects = $ctxt->object->vlesubjects();
-    $doc_data->{context}->{object}->{subjectset} = { subject => \@subjects } if ( !($subjects[0] eq undef ) );
-
-    my @publications = $ctxt->object->vlepublications();
-    $doc_data->{context}->{object}->{publicationset} = { publication => \@publications };
-
-    my $meta = $ctxt->object->vlemeta();
-    $doc_data->{context}->{object}->{meta} = $meta;
-
-    my @vlsubjects = $ctxt->object->vlsubjects();
-    $doc_data->{context}->{vlsubjects} = { subject => \@vlsubjects } if scalar @vlsubjects;
-
-    my @vlkeywords = $ctxt->object->vlkeywords();
-    $doc_data->{context}->{vlkeywords} = { keyword => \@vlkeywords } if scalar @vlkeywords;
+  
+    $self->_insert_vle_common($ctxt, $doc_data);
+  
+    if ( $ctxt->properties->application->style() =~ /^edit/) { 
     
-    my @vlauthors = $ctxt->object->vlauthors();
-    $doc_data->{context}->{vlauthors} = { author => \@vlauthors } if scalar @vlauthors;
-
+        my @vlsubjects = $ctxt->object->vlsubjects();
+        $doc_data->{context}->{vlsubjects} = { subject => \@vlsubjects } if scalar @vlsubjects;
+    
+        my @vlkeywords = $ctxt->object->vlkeywords();
+        $doc_data->{context}->{vlkeywords} = { keyword => \@vlkeywords } if scalar @vlkeywords;
+    
+        my @vlauthors = $ctxt->object->vlauthors();
+        $doc_data->{context}->{vlauthors} = { author => \@vlauthors } if scalar @vlauthors;
+    }
 
     return $doc_data;
 }
-
+    
 1;
