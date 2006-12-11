619c672bb8c122121b482ad63a4869dd35939eb3
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2006-12-11 09:42:23 +0000

    + correct presumable typo

--- xims/upgrade/1.2_1.3/upgrade-pg-vlibrary.sql
+++ xims/upgrade/1.2_1.3/upgrade-pg-vlibrary.sql
@@ -1,7 +1,7 @@
 -- Add the document_id of a VLibrary to the subject
 
 ALTER TABLE cilib_subjects ADD COLUMN document_id int4;
-ALTER TABLE cilib_subjects ALTER COLUMN id SET NOT NULL;
+ALTER TABLE cilib_subjects ALTER COLUMN document_id SET NOT NULL;
 ALTER TABLE cilib_subjects ADD FOREIGN KEY (document_id) REFERENCES ci_documents (id) ON DELETE CASCADE;
 ALTER TABLE cilib_subjects ADD CONSTRAINT cilib_subjects_unique_key UNIQUE(document_id, name);
 
