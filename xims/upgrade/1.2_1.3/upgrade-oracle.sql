c5328c0cee45a567776687dcd0fe961d5e488407
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2006-12-11 10:30:15 +0000

    + jokars DDL-changes for oracle

--- xims/upgrade/1.2_1.3/upgrade-oracle.sql
+++ xims/upgrade/1.2_1.3/upgrade-oracle.sql
@@ -1,3 +1,8 @@
 PROMPT Extending cisimpledb_member_properties.regex column
 ALTER TABLE cisimpledb_member_properties MODIFY (regex VARCHAR2(2048))
 /
+
+PROMPT Adding new attribute ci_content.status_checked_timestamp
+ALTER TABLE CI_CONTENT ADD STATUS_CHECKED_TIMESTAMP DATE
+/
+
