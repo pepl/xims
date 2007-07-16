fdb93afbf23ccaf28564be8be9b5dc6c0082f2e0
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2007-07-16 13:49:50 +0000

    + new attribute DELETIONMARK_TIMESTAMP + Object.pm cleanup w/ perltidy

--- trunk/xims/upgrade/1.2_1.3/upgrade-oracle.sql
+++ trunk/xims/upgrade/1.2_1.3/upgrade-oracle.sql
@@ -6,3 +6,7 @@ PROMPT Adding new attribute ci_content.status_checked_timestamp
 ALTER TABLE CI_CONTENT ADD STATUS_CHECKED_TIMESTAMP DATE
 /
 
+PROMPT Adding new attribute ci_content.deletionmark_timestamp
+ALTER TABLE CI_CONTENT ADD DELETIONMARK_TIMESTAMP DATE
+/
+
--- trunk/xims/upgrade/1.2_1.3/upgrade-pg.sql
+++ trunk/xims/upgrade/1.2_1.3/upgrade-pg.sql
@@ -7,6 +7,9 @@ ALTER TABLE cisimpledb_member_properties RENAME nregex TO regex;
 \echo Adding ci_content.status_checked_timestamp
 ALTER TABLE ci_content ADD status_checked_timestamp TIMESTAMP(0) WITHOUT TIME ZONE;
 
+\echo Adding ci_content.deletionmark_timestamp
+ALTER TABLE ci_content ADD deletionmark_timestamp TIMESTAMP(0) WITHOUT TIME ZONE;
+
 DELETE FROM ci_object_privs_granted WHERE content_id NOT IN (select id from ci_content);
 ALTER TABLE ci_object_privs_granted ADD CONSTRAINT OPG_USR_GRANTEE_FK FOREIGN KEY (GRANTEE_ID) REFERENCES CI_USERS_ROLES (ID) ON DELETE CASCADE;
 ALTER TABLE ci_object_privs_granted ADD CONSTRAINT OPG_USR_GRANTOR_FK FOREIGN KEY (GRANTOR_ID) REFERENCES CI_USERS_ROLES (ID) ON DELETE CASCADE;
