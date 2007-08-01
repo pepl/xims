9da86a6b887a6b1a0d536ef04fc6083cd4d8de02
jerboaa <jerboaa@118f944d-ca0e-0410-82e1-b214406b748f>
2007-08-01 12:45:22 +0000

    add optional switch for XIMS-box build

--- trunk/xims/sql/Pg/defaultdata.sql
+++ trunk/xims/sql/Pg/defaultdata.sql
@@ -232,12 +232,17 @@ INSERT INTO ci_documents ( id, parent_id, object_type_id, data_format_id, depart
 INSERT INTO ci_content ( id, document_id, title, language_id, last_modified_by_id, owned_by_id, created_by_id )
        VALUES ( nextval('ci_content_id_seq'), 1, 'root', 2, 2, 2, 2);
 
--- add SiteRoot "xims" with SiteRoot URL '/ximspubroot/xims'
-INSERT INTO ci_documents ( id, parent_id, object_type_id, data_format_id, department_id, location, position, location_path )
-       VALUES (nextval('ci_documents_id_seq'), 1, 19, 31, 2, 'xims', 1, '/xims' );
-INSERT INTO ci_content ( id, document_id, title, language_id, last_modified_by_id, owned_by_id, created_by_id )
-       VALUES (nextval('ci_content_id_seq') , 2, '/ximspubroot/xims', 2, 2, 2, 2);
+---------------------------------------------------------
+-- add default data content
+--
+-- for a manual XIMS install we only create two SiteRoots
+-- for a the XIMS-box we include more default data
+--
+-- we have a manual XIMS install (via XIMS-installer)
+\i defaultdata-content.sql
 
+-- the following line is for building a XIMS-box
+--\i xims-box-defaultdata-content.sql
 
 \echo inserting into ci_bookmarks...
 -- add default bookmarks
