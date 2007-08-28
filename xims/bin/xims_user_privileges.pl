dbbdeb0222d80f96261af2fa188477bd601f76f1
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2007-08-28 10:16:23 +0000

    + perltidy + add feature to alter grants for multiple users in one run

--- xims/bin/xims_user_privileges.pl
+++ xims/bin/xims_user_privileges.pl
@@ -7,8 +7,10 @@
 use strict;
 
 my $xims_home = $ENV{'XIMS_HOME'} || '/usr/local/xims';
-die "\nWhere am I?\n\nPlease set the XIMS_HOME environment variable if you\ninstall into a different location than /usr/local/xims\n" unless -f "$xims_home/Makefile";
-use lib ($ENV{'XIMS_HOME'} || '/usr/local/xims')."/lib";
+die
+    "\nWhere am I?\n\nPlease set the XIMS_HOME environment variable if you\ninstall into a different location than /usr/local/xims\n"
+    unless -f "$xims_home/Makefile";
+use lib ( $ENV{'XIMS_HOME'} || '/usr/local/xims' ) . "/lib";
 
 use XIMS;
 use XIMS::User;
@@ -17,11 +19,13 @@ use XIMS::ObjectPriv;
 use XIMS::Term;
 use Getopt::Std;
 
+#use Data::Dumper;
+
 my %args;
-getopts('herd:u:p:l:g:', \%args);
+getopts( 'herd:u:p:l:g:', \%args );
 
 my $term = XIMS::Term->new( debuglevel => $args{d} );
-print $term->banner( "User Privilege Manager" );
+print $term->banner("User Privilege Manager");
 
 if ( $args{h} ) {
     print usage();
@@ -32,27 +36,41 @@ unless ( $args{g} and $ARGV[0] ) {
     die usage();
 }
 
-my $user = $term->authenticate( %args );
-die "Could not authenticate user '".$args{u}."'\n" unless $user and $user->id();
+my $user = $term->authenticate(%args);
+die "Could not authenticate user '" . $args{u} . "'\n"
+    unless $user and $user->id();
 
-my $object = XIMS::Object->new( path => $ARGV[0], marked_deleted => undef, User => $user );
-die "Could not find object '".$ARGV[0]."'\n" unless $object and $object->id;
+my $object = XIMS::Object->new(
+    path           => $ARGV[0],
+    marked_deleted => undef,
+    User           => $user
+);
+die "Could not find object '" . $ARGV[0] . "'\n"
+    unless $object and $object->id;
 
-my $privmask = $user->object_privmask( $object );
-die "Access Denied. You do not have privileges to manage privileges for '".$ARGV[0]."'\n" unless $privmask and ($privmask & XIMS::Privileges::GRANT());
+my $privmask = $user->object_privmask($object);
+die "Access Denied. You do not have privileges to manage privileges for '"
+    . $ARGV[0] . "'\n"
+    unless $privmask and ( $privmask & XIMS::Privileges::GRANT() );
 
-my $grantee = XIMS::User->new( name => $args{g});
-die "Grantee '".$args{g}."' could not be found.\n" unless $grantee and $grantee->id();
+my @grantees;
+
+foreach ( split( /\s*,\s*/, $args{g} ) ) {
+    my $grantee = XIMS::User->new( name => $_ );
+    die "Grantee '" . $_ . "' could not be found.\n"
+        unless $grantee and $grantee->id();
+    push @grantees, $grantee;
+}
 
 my $privnames = $args{l} || 'VIEW';
 my $gprivmask;
 if ( my $privlist = $args{l} ) {
     my @privileges = split ',', $privlist;
     my $tpriv;
-    foreach my $priv ( @privileges ) {
+    foreach my $priv (@privileges) {
         no strict 'refs';
         $tpriv = &{"XIMS::Privileges::$priv"};
-        $gprivmask |= $tpriv if ($tpriv and $tpriv > 0);
+        $gprivmask |= $tpriv if ( $tpriv and $tpriv > 0 );
     }
 }
 else {
@@ -64,18 +82,26 @@ $method = 'revoke' if $args{e};
 
 my $path = $object->location_path();
 
-my $total = 0;
-my $successful = 0;
-my $failed = 0;
-my $ungranted = 0;
+my $total          = 0;
+my $successful     = 0;
+my $failed         = 0;
+my $ungranted      = 0;
 my $iterate_anyway = 0;
 
 if ( $method eq 'grant' ) {
-    if ( $object->grant_user_privileges(
-                                        grantee  => $grantee,
-                                        grantor  => $user,
-                                        privmask => $gprivmask
-                                           )) {
+    my $ok = 1;
+
+    foreach my $grantee (@grantees) {
+
+        #$ok &=
+        $object->grant_user_privileges(
+            grantee  => $grantee,
+            grantor  => $user,
+            privmask => $gprivmask
+        );
+    }
+
+    if ($ok) {
         print "Granted $privnames on '$path'.\n";
         $successful++;
     }
@@ -86,34 +112,55 @@ if ( $method eq 'grant' ) {
     $total++;
 }
 else {
-    my $privs_object = XIMS::ObjectPriv->new( grantee_id => $grantee->id, content_id => $object->id() );
-    if ( $privs_object and $privs_object->delete() ) {
-        print "Revoked grantee from '$path'.\n";
+
+    my $ok = 1;
+
+    foreach my $grantee (@grantees) {
+
+        my $privs_object = XIMS::ObjectPriv->new(
+            grantee_id => $grantee->id,
+            content_id => $object->id()
+        );
+        $ok &= ( $privs_object and $privs_object->delete() );
+    }
+
+    if ($ok) {
+        print "Revoked grantee(s)) from '$path'.\n";
         $successful++;
     }
     else {
-        print "Could not revoke grantee from '$path'.\n";
+        print "Could not revoke grantee(s) from '$path'.\n";
         $failed++;
-        # for recursive revocation of grants if the upmost object itself 
+
+        # for recursive revocation of grants if the upmost object itself
         # lacks the acl-entry to be removed
         $iterate_anyway = 1;
     }
     $total++;
 }
 
-if ( ($successful or $iterate_anyway) and $args{r} ) {
+if ( ( $successful or $iterate_anyway ) and $args{r} ) {
     my $desc_privmask;
-    my $iterator = $object->descendants_granted( User => $user, marked_deleted => undef );
+    my $iterator = $object->descendants_granted(
+        User           => $user,
+        marked_deleted => undef
+    );
     while ( my $desc = $iterator->getNext() ) {
-        $desc_privmask = $user->object_privmask( $desc );
-        $path = $desc->location_path();
+        $desc_privmask = $user->object_privmask($desc);
+        $path          = $desc->location_path();
         if ( $desc_privmask & XIMS::Privileges::GRANT() ) {
             if ( $method eq 'grant' ) {
-                if ( $desc->grant_user_privileges(
-                                                grantee  => $grantee,
-                                                grantor  => $user,
-                                                privmask => $gprivmask
-                                                  ) ) {
+                my $ok = 1;
+
+                foreach my $grantee (@grantees) {
+                    $ok &= $desc->grant_user_privileges(
+                        grantee  => $grantee,
+                        grantor  => $user,
+                        privmask => $gprivmask
+                    );
+                }
+
+                if ($ok) {
                     print "Granted $privnames on '$path'.\n";
                     $successful++;
                 }
@@ -124,8 +171,16 @@ if ( ($successful or $iterate_anyway) and $args{r} ) {
                 $total++;
             }
             else {
-                my $privs_object = XIMS::ObjectPriv->new( grantee_id => $grantee->id, content_id => $desc->id() );
-                if ( $privs_object and $privs_object->delete() ) {
+                my $ok = 1;
+                foreach my $grantee (@grantees) {
+                    my $privs_object = XIMS::ObjectPriv->new(
+                        grantee_id => $grantee->id,
+                        content_id => $desc->id()
+                    );
+                    $ok &= ( $privs_object and $privs_object->delete() );
+                }
+
+                if ($ok) {
                     print "Revoked grantee from '$path'.\n";
                     $successful++;
                 }
@@ -137,7 +192,8 @@ if ( ($successful or $iterate_anyway) and $args{r} ) {
             }
         }
         else {
-            print "Insufficient privileges to $method privileges on '$path'.\n";
+            print
+                "Insufficient privileges to $method privileges on '$path'.\n";
             $total++;
             $ungranted++;
         }
@@ -174,6 +230,4 @@ sub usage {
 
 *;
 }
-
-
 
