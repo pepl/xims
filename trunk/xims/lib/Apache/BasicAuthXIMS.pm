47212dd61c41efef643e409d4391292c98fb2a02
cdab <cdab@118f944d-ca0e-0410-82e1-b214406b748f>
2007-10-22 08:49:34 +0000

    changed document pod

--- trunk/xims/lib/Apache/BasicAuthXIMS.pm
+++ trunk/xims/lib/Apache/BasicAuthXIMS.pm
@@ -1,7 +1,25 @@
-# Copyright (c) 2002-2006 The XIMS Project.
-# See the file "LICENSE" for information and conditions for use, reproduction,
-# and distribution of this work, and for a DISCLAIMER OF ALL WARRANTIES.
-# $Id$
+
+=head1 NAME
+
+Apache::BasicAuthXIMS -- A module doing basic authentication for XIMS.
+
+=head1 VERSION
+
+$Id:$
+
+=head1 SYNOPSIS
+
+use Apache::BasicAuthXIMS;
+
+=head1 DESCRIPTION
+
+This module allows the use of HTTP Basic Authentication to restrict access by
+looking up users in the given providers.
+
+=head1 SUBROUTINES/METHODS
+
+=cut
+
 package Apache::BasicAuthXIMS;
 
 use strict;
@@ -15,35 +33,38 @@ use XIMS::Auth;
 
 our ($VERSION) = ( q$Revision$ =~ /\s+(\d+)\s*$/ );
 
-##
-#
-# SYNOPSIS
-#    handler
-#
-# PARAMETERS
-#    $r: request-object
-#
-# RETURNVALUES
-#    One of 'OK', 'AUTH_REQUIRED, 'SERVER_ERROR'
-#
-# DESCRIPTION
-#    Simple Basic Authentication handler using XIMS::Auth
-#    Example Usage:
-#
-#        <Locationmatch "^/godav*">
-#            #RewriteEngine On
-#            #RewriteCond %{SERVER_PORT}!443
-#            #RewriteRule ^(.*)$ https://host/godav/$1 [R,L]
-#            SetHandler perl-script
-#            PerlHandler goxims
-#
-#            # Authentication Realm and Type (only Basic supported)
-#            AuthName "XIMS Authentication"
-#            AuthType Basic
-#            PerlAuthenHandler Apache::BasicAuthXIMS
-#            #PerlSetEnv ORACLE_HOME /od4/oracle/product/9.2.0.1.0
-#        </Locationmatch>
-#
+
+
+=head2    handler
+
+=head3 Parameter
+
+    $r: request-object
+
+ RETURNVALUES
+    One of 'OK', 'AUTH_REQUIRED, 'SERVER_ERROR'
+
+=head3 Description
+
+Simple Basic Authentication handler using XIMS::Auth
+Example Usage:
+
+  <Locationmatch "^/godav*">
+     #RewriteEngine On
+     #RewriteCond %{SERVER_PORT}!443
+     #RewriteRule ^(.*)$ https://host/godav/$1 [R,L]
+     SetHandler perl-script
+     PerlHandler goxims
+
+    # Authentication Realm and Type (only Basic supported)
+     AuthName "XIMS Authentication"
+     AuthType Basic
+     PerlAuthenHandler Apache::BasicAuthXIMS
+     #PerlSetEnv ORACLE_HOME /od4/oracle/product/9.2.0.1.0
+  </Locationmatch>
+
+=cut
+
 sub handler {
     my $r = shift;
 
@@ -86,4 +107,39 @@ sub handler {
 }
 
 1;
+
+
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+   httpd_conf, ximshttpd.conf
+
+=head1 BUGS AND LIMITATION
+
+Grep the source file for: XXX, TODO, ITS_A_HACK_ALARM.
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2002-2007 The XIMS Project.
+
+See the file "LICENSE" for information and conditions for use,
+reproduction, and distribution of this work, and for a DISCLAIMER OF ALL
+WARRANTIES.
+
+=cut
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   cperl-close-paren-offset: -4
+#   cperl-continued-statement-offset: 4
+#   cperl-indent-level: 4
+#   cperl-indent-parens-as-block: t
+#   cperl-merge-trailing-else: nil
+#   cperl-tab-always-indent: t
+#   fill-column: 78
+#   indent-tabs-mode: nil
+# End:
+# ex: set ts=4 sr sw=4 tw=78 ft=perl et :
 
