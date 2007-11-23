42b69979dabe4686d2ccae264a5dcc3ac4fa1e1f
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2007-11-23 08:46:43 +0000

    + perltidy and little style changes.

--- xims/lib/XIMS/User.pm
+++ xims/lib/XIMS/User.pm
@@ -5,13 +5,14 @@ XIMS::User -- A .... doing bla, bla, bla. (short)
 
 =head1 VERSION
 
-$Id:$
+$Id$
 
 =head1 SYNOPSIS
 
     use XIMS::User;
 
 =head1 DESCRIPTION
+
 
 This module bla bla
 
@@ -28,7 +29,7 @@ use XIMS::Bookmark;
 use Digest::MD5 qw( md5_hex );
 
 our ($VERSION) = ( q$Revision$ =~ /\s+(\d+)\s*$/ );
-our @Fields = @{XIMS::Names::property_interface_names( resource_type() )};
+our @Fields = @{ XIMS::Names::property_interface_names( resource_type() ) };
 
 sub fields {
     return @Fields;
@@ -40,13 +41,15 @@ sub resource_type {
     return $rt;
 }
 
-__PACKAGE__->mk_accessors( @Fields );
+__PACKAGE__->mk_accessors(@Fields);
 
 sub validate_password {
     XIMS::Debug( 5, "called" );
-    my $self = shift;
+    my $self       = shift;
     my $raw_passwd = shift;
-    if ( defined( $raw_passwd ) and $self->password() eq Digest::MD5::md5_hex( $raw_passwd ) ) {
+    if ( defined($raw_passwd)
+        and $self->password() eq Digest::MD5::md5_hex($raw_passwd) )
+    {
         return 1;
     }
     return;
@@ -55,13 +58,13 @@ sub validate_password {
 sub creatable_object_types {
     XIMS::Debug( 5, "called" );
     my $self = shift;
-    return $self->objecttype_privileges( @_ );
+    return $self->objecttype_privileges(@_);
 }
 
 sub object_privmask {
     XIMS::Debug( 5, "called" );
-    my $self = shift;
-    my $object = shift;
+    my $self     = shift;
+    my $object   = shift;
     my $explicit = shift;
     my $privmask = 0;
 
@@ -69,18 +72,24 @@ sub object_privmask {
 
     my @id_list = $self->id();
     push( @id_list, $self->role_ids() ) unless $explicit;
+
     #warn "IDS: " . Dumper( \@id_list );
 
-    my @priv_data = $self->data_provider->getObjectPriv( content_id => $object->id(),
-                                                         grantee_id => \@id_list,
-                                                         properties => [ 'privilege_mask' ] );
+    my @priv_data = $self->data_provider->getObjectPriv(
+        content_id => $object->id(),
+        grantee_id => \@id_list,
+        properties => ['privilege_mask']
+    );
 
     #warn "privs returned: " . Dumper( @priv_data );
-    if ( scalar( @priv_data ) > 0 ) {
-        foreach my $priv ( @priv_data ) {
-            return if $priv->{'objectpriv.privilege_mask'} == 0; # return if we got a specific lockout
+    if ( scalar(@priv_data) > 0 ) {
+        foreach my $priv (@priv_data) {
+            return
+              if $priv->{'objectpriv.privilege_mask'} ==
+              0;    # return if we got a specific lockout
             $privmask = $privmask | $priv->{'objectpriv.privilege_mask'};
         }
+
         #warn "returning privmask: $privmask";
         return $privmask;
     }
@@ -91,33 +100,42 @@ sub object_privmask {
 
 sub object_privileges {
     XIMS::Debug( 5, "called" );
-    my $self = shift;
-    my $object = shift;
+    my $self     = shift;
+    my $object   = shift;
     my $explicit = shift;
-    my $mask = $self->object_privmask( $object, $explicit );
+    my $mask     = $self->object_privmask( $object, $explicit );
+
     return () unless $mask;
-    my $privs_hash = XIMS::Helpers::privmask_to_hash( $mask );
-    return %{$privs_hash} if ref( $privs_hash ) eq 'HASH' and keys( %{$privs_hash} ) > 0;
+
+    my $privs_hash = XIMS::Helpers::privmask_to_hash($mask);
+
+    if ( ref($privs_hash) eq 'HASH' and keys( %{$privs_hash} ) > 0 ) {
+        return %{$privs_hash};
+    }
+
     return ();
 }
 
-#sub grant_object_privilege {
-#    my $self = shift;
-#    my %args = @_;
-#    my $obj = $args{Object};
-#    die "must have a object to grant to" unless defined( $object );
-#
-#
-#
-#}
+# sub grant_object_privilege {
+#     my $self = shift;
+#     my %args = @_;
+#     my $obj = $args{Object};
+#     die "must have a object to grant to" unless defined( $object );
+# }
 
 sub system_privileges {
     XIMS::Debug( 5, "called" );
     my $self = shift;
     my $mask = $self->system_privs_mask();
+
     return () unless $mask;
-    my $privs_hash = XIMS::Helpers::system_privmask_to_hash( $mask );
-    return %{$privs_hash} if ref( $privs_hash ) eq 'HASH' and keys( %{$privs_hash} ) > 0;
+
+    my $privs_hash = XIMS::Helpers::system_privmask_to_hash($mask);
+
+    if ( ref($privs_hash) eq 'HASH' and keys( %{$privs_hash} ) > 0 ) {
+        return %{$privs_hash};
+    }
+
     return ();
 }
 
@@ -125,10 +143,17 @@ sub dav_otprivileges {
     XIMS::Debug( 5, "called" );
     my $self = shift;
     my $mask = $self->dav_otprivs_mask();
+
     return () unless $mask;
+
     my @object_types = $self->data_provider->object_types( is_davgetable => 1 );
-    my $privs_hash = XIMS::Helpers::dav_otprivmask_to_hash( $mask, \@object_types );
-    return %{$privs_hash} if ref( $privs_hash ) eq 'HASH' and keys( %{$privs_hash} ) > 0;
+    my $privs_hash =
+      XIMS::Helpers::dav_otprivmask_to_hash( $mask, \@object_types );
+
+    if ( ref($privs_hash) eq 'HASH' and keys( %{$privs_hash} ) > 0 ) {
+        return %{$privs_hash};
+    }
+
     return ();
 }
 
@@ -136,14 +161,16 @@ sub default_bookmark {
     XIMS::Debug( 5, "called" );
     my $self = shift;
     my $bmk = XIMS::Bookmark->new( owner_id => $self->id(), stdhome => 1 );
-    if ( $bmk ) {
-         return $bmk;
+
+    if ($bmk) {
+        return $bmk;
     }
     else {
         my $default_role = $self->roles_granted( default_role => 1 );
         return unless $default_role;
-        $bmk = XIMS::Bookmark->new( owner_id => $default_role->id(), stdhome => 1 );
-        if ( $bmk ) {
+        $bmk =
+          XIMS::Bookmark->new( owner_id => $default_role->id(), stdhome => 1 );
+        if ($bmk) {
             return $bmk;
         }
     }
@@ -154,18 +181,19 @@ sub bookmarks {
     my $self = shift;
     my %args = @_;
 
-    my @userid = ( $self->id() );
+    my @userid            = ( $self->id() );
     my @roles_granted_ids = map { $_->id() } $self->roles_granted();
-    my $explicit_only = delete $args{explicit_only};
+    my $explicit_only     = delete $args{explicit_only};
     push @userid, @roles_granted_ids unless $explicit_only;
 
     my %params;
-    $params{owner_id} = \@userid;
-    $params{stdhome} = $args{stdhome} if exists $args{stdhome};
+    $params{owner_id}   = \@userid;
+    $params{stdhome}    = $args{stdhome} if exists $args{stdhome};
     $params{content_id} = $args{content_id} if exists $args{content_id};
 
-    my @bookmarks_data = $self->data_provider->getBookmark( %params );
+    my @bookmarks_data = $self->data_provider->getBookmark(%params);
     my @bookmarks = map { XIMS::Bookmark->new->data( %{$_} ) } @bookmarks_data;
+
     #warn "bookmarks" . Dumper( \@bookmarks);
     return wantarray ? @bookmarks : $bookmarks[0];
 }
@@ -180,8 +208,9 @@ sub role_ids {
 
     # return cached role_ids if no args are given
     if ( keys %args == 0 and exists $self->{role_ids} ) {
+
         #warn "returning cached role_ids";
-        return @{$self->{role_ids}};
+        return @{ $self->{role_ids} };
     }
 
     my @current_ids = ();
@@ -196,11 +225,12 @@ sub role_ids {
         $conditions->{'userpriv.role_master'} = 1;
     }
 
-    while ( 1 ) {
+    while (1) {
         $conditions->{'userpriv.grantee_id'} = \@current_ids;
-        my $id_data = $self->data_provider->{Driver}->get( properties => { 'userpriv.id' => 1 },
-                                                           conditions => $conditions
-                                                         );
+        my $id_data = $self->data_provider->{Driver}->get(
+            properties => { 'userpriv.id' => 1 },
+            conditions => $conditions
+        );
         if ( scalar( @{$id_data} ) > 0 ) {
             @current_ids = map { values %{$_} } @{$id_data};
             map { $id_list{$_} = 1 } @current_ids;
@@ -212,6 +242,7 @@ sub role_ids {
     }
 
     @role_ids = keys %id_list;
+
     # cache role ids if no args are given
     $self->{role_ids} = \@role_ids if keys %args == 0;
 
@@ -223,10 +254,11 @@ sub roles_granted {
     my $self = shift;
     my %args = @_;
 
-    my @role_ids = $self->role_ids( %args );
-    return () unless scalar( @role_ids ) > 0 ;
+    my @role_ids = $self->role_ids(%args);
+    return () unless scalar(@role_ids) > 0;
     my @roles_data = $self->data_provider->getUser( id => \@role_ids );
     my @roles = map { XIMS::User->new->data( %{$_} ) } @roles_data;
+
     #warn "roles" . Dumper( \@roles ) . "\n";
     return wantarray ? @roles : $roles[0];
 }
@@ -241,33 +273,47 @@ sub objecttype_privileges {
     my $privs;
 
     if ( $self->admin() ) {
-        foreach my $type ( @all_types ) {
+        foreach my $type (@all_types) {
             $type->{can_create} = 1;
         }
         return @all_types;
     }
     else {
         my @id_list = ( $self->role_ids(), $self->id() );
+
         #warn "OT IDS: " . Dumper( \@id_list );
-        my @privs = $self->data_provider->getObjectTypePriv( grantee_id => \@id_list );
+        my @privs =
+          $self->data_provider->getObjectTypePriv( grantee_id => \@id_list );
+
         #warn "privs returned: " . Dumper( \@privs );
 
-        my %oprivs = map { $_->{'objecttypepriv.object_type_id'} => [ $_->{'objecttypepriv.grantee_id'}, $_->{'objecttypepriv.grantor_id'} ] } @privs;
+        my %oprivs = map {
+            $_->{'objecttypepriv.object_type_id'} => [
+                $_->{'objecttypepriv.grantee_id'},
+                $_->{'objecttypepriv.grantor_id'}
+              ]
+        } @privs;
 
         if ( exists $args{add_oprivdata} ) {
-            # make sure explicit object type grants have precedence (two loops are faster than sorting the array...)
+
+            # make sure explicit object type grants have precedence (two loops
+            # are faster than sorting the array...)
             my $userid = $self->id();
-            for ( @privs ) {
-                $oprivs{'objecttypepriv.object_type_id'} = [ $_->{'objecttypepriv.grantee_id'}, $_->{'objecttypepriv.grantor_id'} ] if $_->{'objecttypepriv.grantee_id'} == $userid;
+            for (@privs) {
+                $oprivs{'objecttypepriv.object_type_id'} = [
+                    $_->{'objecttypepriv.grantee_id'},
+                    $_->{'objecttypepriv.grantor_id'}
+                  ]
+                  if $_->{'objecttypepriv.grantee_id'} == $userid;
             }
         }
 
-        foreach my $type ( @all_types ) {
-            if ( exists $oprivs{$type->{id}} ) {
+        foreach my $type (@all_types) {
+            if ( exists $oprivs{ $type->{id} } ) {
                 $type->{can_create} = 1;
                 if ( exists $args{add_oprivdata} ) {
-                    $type->{grantee_id} = $oprivs{$type->{id}}->[0];
-                    $type->{grantor_id} = $oprivs{$type->{id}}->[1];
+                    $type->{grantee_id} = $oprivs{ $type->{id} }->[0];
+                    $type->{grantor_id} = $oprivs{ $type->{id} }->[1];
                 }
             }
             else {
@@ -277,6 +323,7 @@ sub objecttype_privileges {
             }
         }
     }
+
     #warn "returning alltypes " . Dumper( \@all_types );
     return @all_types;
 }
@@ -290,25 +337,28 @@ sub dav_object_types_granted {
     return @object_types if $self->admin();
 
     my $privmask = $self->dav_otprivs_mask();
-    # cast $privmask to an integer, so that the bitwise operation will work as expected
+
+    # cast $privmask to an integer, so that the bitwise operation will work as
+    # expected
     1 if $privmask == 1;
 
     # Add otprivs from granted roles
     my @roles = $self->roles_granted();
-    foreach my $role ( @roles ) {
+    foreach my $role (@roles) {
         $privmask |= $role->dav_otprivs_mask();
     }
+
     # warn "Aggregated privmask: $privmask";
 
     my @object_types_granted = ();
-    foreach my $object_type ( @object_types ) {
-        push ( @object_types_granted, $object_type ) if $privmask & $object_type->davprivval();
+    foreach my $object_type (@object_types) {
+        push( @object_types_granted, $object_type )
+          if $privmask & $object_type->davprivval();
     }
 
     #warn "returning " . Dumper( \@object_types_granted );
     return @object_types_granted;
 }
-
 
 # use XIMS::UserPriv here?
 sub grant_role_privileges {
@@ -316,32 +366,52 @@ sub grant_role_privileges {
     my %args = @_;
 
     die "must have a grantor, a grantee and a role"
-        unless defined( $args{grantor} ) and defined( $args{grantee} ) and defined( $args{role} );
+      unless defined( $args{grantor} )
+      and defined( $args{grantee} )
+      and defined( $args{role} );
 
-    # these allow the User-based args to be either a User object or the id() of one.
+    # these allow the User-based args to be either a User object or the id()
+    # of one.
 
-    my $grantee_id = ( ref( $args{grantee} ) && $args{grantee}->isa('XIMS::User') ) ? $args{grantee}->id() : $args{grantee};
-    my $grantor_id = ( ref( $args{grantor} ) && $args{grantor}->isa('XIMS::User') ) ? $args{grantor}->id() : $args{grantor};
-    my $role_id    = ( ref( $args{role}    ) && $args{role}->isa('XIMS::User') )    ? $args{role}->id()    : $args{role};
+    my $grantee_id =
+      ( ref( $args{grantee} ) && $args{grantee}->isa('XIMS::User') )
+      ? $args{grantee}->id()
+      : $args{grantee};
+    my $grantor_id =
+      ( ref( $args{grantor} ) && $args{grantor}->isa('XIMS::User') )
+      ? $args{grantor}->id()
+      : $args{grantor};
+    my $role_id =
+      ( ref( $args{role} ) && $args{role}->isa('XIMS::User') )
+      ? $args{role}->id()
+      : $args{role};
 
     my $serialization_method;
 
-    if ( $self->data_provider->getUserPriv( grantee_id => $grantee_id, id => $role_id, grantor_id => $grantor_id ) ) {
+    if (
+        $self->data_provider->getUserPriv(
+            grantee_id => $grantee_id,
+            id         => $role_id,
+            grantor_id => $grantor_id
+        )
+      )
+    {
         $serialization_method = 'updateUserPriv';
     }
     else {
         $serialization_method = 'createUserPriv';
     }
 
-    my %params = ( grantor_id => $grantor_id,
-                   grantee_id => $grantee_id,
-                   id => $role_id
-                 );
+    my %params = (
+        grantor_id => $grantor_id,
+        grantee_id => $grantee_id,
+        id         => $role_id
+    );
 
     $params{default_role} = $args{default_role} if defined $args{default_role};
     $params{role_master}  = $args{role_master}  if defined $args{role_master};
 
-    return $self->data_provider->$serialization_method( %params );
+    return $self->data_provider->$serialization_method(%params);
 }
 
 1;
@@ -351,20 +421,6 @@ __END__
 =head1 DIAGNOSTICS
 
 Look at the F<error_log> file for messages.
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-in F<httpd.conf>: yadda, yadda...
-
-Optional section , remove if bogus
-
-=head1 DEPENDENCIES
-
-Optional section, remove if bogus.
-
-=head1 INCOMPATABILITIES
-
-Optional section, remove if bogus.
 
 =head1 BUGS AND LIMITATION
 
