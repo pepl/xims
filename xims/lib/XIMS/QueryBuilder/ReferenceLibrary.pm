5523324e275c266f920bc03af3996ee2c33fb61f
cdab <cdab@118f944d-ca0e-0410-82e1-b214406b748f>
2008-01-07 09:30:22 +0000

    pod changed

--- xims/lib/XIMS/QueryBuilder/ReferenceLibrary.pm
+++ xims/lib/XIMS/QueryBuilder/ReferenceLibrary.pm
@@ -5,7 +5,7 @@ XIMS::QueryBuilder::ReferenceLibrary -- A .... doing bla, bla, bla. (short)
 
 =head1 VERSION
 
-$Id:$
+$Id$
 
 =head1 SYNOPSIS
 
@@ -43,7 +43,8 @@ our ($VERSION) = ( q$Revision$ =~ /\s+(\d+)\s*$/ );
 =head3 Description
 
 
-Helper method that fills up $self->{criteria}, $self->{properties}, and $self->{order} on success
+Helper method that fills up $self->{criteria}, $self->{properties},
+and $self->{order} on success
 
 
 =cut
