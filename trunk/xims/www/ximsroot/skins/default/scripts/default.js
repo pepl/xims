0c1ab07860d837e01d41d238e133a58f03267983
pepl <pepl@118f944d-ca0e-0410-82e1-b214406b748f>
2008-12-25 21:24:56 +0000

    Sprites for options and df list icons

--- trunk/xims/www/ximsroot/skins/default/scripts/default.js
+++ trunk/xims/www/ximsroot/skins/default/scripts/default.js
@@ -1,113 +1,9 @@
 /*
-# Copyright (c) 2002-2006 The XIMS Project.
+# Copyright (c) 2002-2008 The XIMS Project.
 # See the file "LICENSE" for information and conditions for use, reproduction,
 # and distribution of this work, and for a DISCLAIMER OF ALL WARRANTIES.
 # $Id$
 */
-
-agent = navigator.userAgent;
-browser = 0;
-selected = '';
-if (agent.indexOf("a/3",1) == 6 || agent.indexOf("a/4",1) == 6 || agent.indexOf("a/5",1) == 6) {browser = 1} else {browser = 0}
-
-// Cache Image
-
-/*
-var preloaded = new Array();
-function preload_images() {
-    for (var i = 0; i < arguments.length; i++){
-        preloaded[i] = document.createElement('img');
-        preloaded[i].setAttribute('src',arguments[i]);
-    };
-};
-
-preload_images(
-    '/ximsroot/skins/default/images/navigate-back.png',
-    '/ximsroot/skins/default/images/navigate-up.png',
-    '/ximsroot/skins/default/images/navigate-forward.png',
-    '/ximsroot/skins/default/images/option_move.png',
-    '/ximsroot/skins/default/images/option_move_over.png',
-    '/ximsroot/skins/default/images/option_move_click.png',
-    '/ximsroot/skins/default/images/option_edit.png',
-    '/ximsroot/skins/default/images/option_edit_over.png',
-    '/ximsroot/skins/default/images/option_edit_click.png',
-    '/ximsroot/skins/default/images/option_delete.png',
-    '/ximsroot/skins/default/images/option_delete_over.png',
-    '/ximsroot/skins/default/images/option_delete_click.png',
-    '/ximsroot/skins/default/images/option_copy.png',
-    '/ximsroot/skins/default/images/option_pub.png',
-    '/ximsroot/skins/default/images/option_acl.png',
-    '/ximsroot/skins/default/images/status_pub.png',
-    '/ximsroot/skins/default/images/status_locked.png',
-    '/ximsroot/images/spacer_white.gif'
-);
-*/
-
-if (browser == 1) {
-    /* Back, Forward, Up Dir */
-
-    back_n = new Image(38,34);
-    back_n.src = "/ximsroot/skins/default/images/navigate-back.png";
-    back_h = new Image(38,34);
-    back_h.src = "/ximsroot/skins/default/images/navigate-back.png";
-    back_s = new Image(38,34);
-    back_s.src = "/ximsroot/skins/default/images/navigate-back.png";
-    back_c = new Image(38,34);
-    back_c.src = "/ximsroot/skins/default/images/navigate-back.png";
-
-    up_n = new Image(31,36);
-    up_n.src = "/ximsroot/skins/default/images/navigate-up.png";
-    up_h = new Image(31,36);
-    up_h.src = "/ximsroot/skins/default/images/navigate-up.png";
-    up_s = new Image(31,36);
-    up_s.src = "/ximsroot/skins/default/images/navigate-up.png";
-    up_c = new Image(31,36);
-    up_c.src = "/ximsroot/skins/default/images/navigate-up.png";
-
-    forward_n = new Image(31,36);
-    forward_n.src = "/ximsroot/skins/default/images/navigate-forward.png";
-    forward_h = new Image(31,36);
-    forward_h.src = "/ximsroot/skins/default/images/navigate-forward.png";
-    forward_s = new Image(31,36);
-    forward_s.src = "/ximsroot/skins/default/images/navigate-forward.png";
-    forward_c = new Image(31,36);
-    forward_c.src = "/ximsroot/skins/default/images/navigate-forward.png";
-
-    move_h = new Image(32,19);
-    move_h.src = "/ximsroot/skins/default/images/option_move_over.png";
-    move_s = new Image(32,19);
-    move_s.src = "/ximsroot/skins/default/images/option_move_click.png";
-    move_c = new Image(32,19);
-    move_c.src = "/ximsroot/skins/default/images/option_move.png";
-
-    edit_h = new Image(32,19);
-    edit_h.src = "/ximsroot/skins/default/images/option_edit_over.png";
-    edit_s = new Image(32,19);
-    edit_s.src = "/ximsroot/skins/default/images/option_edit_click.png";
-    edit_c = new Image(32,19);
-    edit_c.src = "/ximsroot/skins/default/images/option_edit.png";
-
-    delete_h = new Image(37,19);
-    delete_h.src = "/ximsroot/skins/default/images/option_delete_over.png";
-    delete_s = new Image(37,19);
-    delete_s.src = "/ximsroot/skins/default/images/option_delete_click.png";
-    delete_c = new Image(37,19);
-    delete_c.src = "/ximsroot/skins/default/images/option_delete.png";
-
-    publish_c = new Image(32,19);
-    publish_c.src = "/ximsroot/skins/default/images/option_pub.png";
-
-    acl_c = new Image(32,19);
-    acl_c.src = "/ximsroot/skins/default/images/option_acl.png";
-}
-
-//Animate Button
-
-function pass(button,action,type,input) {
-    if (browser == 1) {
-        document.images[button].src = eval(action + "_" + type + ".src");
-    }
-}
 
 /* function for inline pop-ups */
 function openCloseInlinePopup(action, fadelayer, popup, selectButton) {
@@ -116,25 +12,25 @@ function openCloseInlinePopup(action, fadelayer, popup, selectButton) {
     /* when no parameter is given as to which button to select,
        select button with id "xims_ilp_btn_select" */
     if (!selectButton) {
-	selBtn = "xims_ilp_btn_select";
+        selBtn = "xims_ilp_btn_select";
     }
     else {
-	selBtn = selectButton;
+        selBtn = selectButton;
     }
     if (action == "open") {
-	// IE hack for ilp-height
-	if ( navigator.userAgent.indexOf("MSIE") != -1 ) {
-	    document.getElementById(fadelayer).style.height = 1000;
-	}
-	displayProperty = "block";
+        // IE hack for ilp-height
+        if ( navigator.userAgent.indexOf("MSIE") != -1 ) {
+            document.getElementById(fadelayer).style.height = 1000;
+        }
+        displayProperty = "block";
     }
     else { 
-	displayProperty = "none";
+        displayProperty = "none";
     }
     document.getElementById(fadelayer).style.display = displayProperty;
     document.getElementById(popup).style.display = displayProperty;
     // finally select default button, in order to avoid a form-submit
     if (action == "open") {
-	document.getElementById(selBtn).focus();
+        document.getElementById(selBtn).focus();
     }
 }
