82af7eacdf2229399342b12449a89c7eec8fdb4b
Michael Kröll <pepl@cpan.org>
2009-01-03 16:45:48 +0000

    Bumped &copy; to 2009

--- xims/lib/XIMS/SAX/Generator/ReferenceLibrary.pm
+++ xims/lib/XIMS/SAX/Generator/ReferenceLibrary.pm
@@ -5,7 +5,7 @@ XIMS::SAX::Generator::ReferenceLibrary -- A .... doing bla, bla, bla. (short)
 
 =head1 VERSION
 
-$Id:$
+$Id$
 
 =head1 SYNOPSIS
 
@@ -148,7 +148,7 @@ Grep the source file for: XXX, TODO, ITS_A_HACK_ALARM.
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) 2002-2007 The XIMS Project.
+Copyright (c) 2002-2009 The XIMS Project.
 
 See the file F<LICENSE> for information and conditions for use, reproduction,
 and distribution of this work, and for a DISCLAIMER OF ALL WARRANTIES.
