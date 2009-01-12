14904be8a6e575f78f7955b714e2028f1c824582
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2009-01-12 11:38:34 +0000

    + Set libxslt's global max_depth limit to 500 (default 250). Needed for   re-positioning in containers with more than 250 objects.

--- trunk/xims/conf/ximsstartup.pl
+++ trunk/xims/conf/ximsstartup.pl
@@ -6,6 +6,9 @@ $ENV{MOD_PERL} or die "not running under mod_perl!";
 use Apache::DBI ();
 use XML::LibXSLT ();
 
+# allow deeper recursion (default 250)
+XML::LibXSLT->max_depth(500);
+
 # DBD preloading, uncomment to suit your needs
 # DBI->install_driver("Pg");
 # DBI->install_driver("Oracle");
