66007a646aeb25e48065f5f5c8bdc9fd17117fab
susannetober <susannetober@118f944d-ca0e-0410-82e1-b214406b748f>
2009-11-04 09:47:10 +0000

    aria- and jquery-ui methods moved from default.js to skin-js-file

--- trunk/xims/www/ximsroot/scripts/default.js
+++ trunk/xims/www/ximsroot/scripts/default.js
@@ -1,11 +1,10 @@
 /*
-# Copyright (c) 2002-2009 The XIMS Project.
-# See the file "LICENSE" for information and conditions for use, reproduction,
-# and distribution of this work, and for a DISCLAIMER OF ALL WARRANTIES.
-# $Id$
-*/
-
-function confirmDelete() {
+ # Copyright (c) 2002-2009 The XIMS Project.
+ # See the file "LICENSE" for information and conditions for use, reproduction,
+ # and distribution of this work, and for a DISCLAIMER OF ALL WARRANTIES.
+ # $Id$
+ */
+function confirmDelete(){
     if (confirm("Are you sure?")) {
         return true
     }
@@ -14,36 +13,34 @@ function confirmDelete() {
     }
 }
 
-function genericWindow( url, width, height ) {
-    var height = ( height == null ) ? "400" : height;
-    var width  = ( width  == null ) ? "400" : width;
+function genericWindow(url, width, height){
+    var height = (height == null) ? "400" : height;
+    var width = (width == null) ? "400" : width;
     
-    newWindow  = window.open( url,
-                              "displayWindow",
-                              "resizable=yes,scrollbars=yes,width="
-                              + width
-                              + ",height="
-                              + height
-                              + ",screenX=100,screenY=300" );
+    newWindow = window.open(url, "displayWindow", "resizable=yes,scrollbars=yes,width=" +
+    width +
+    ",height=" +
+    height +
+    ",screenX=100,screenY=300");
 }
 
-function previewWindow(url) {
-    newWindow = window.open( url, "displayWindow", "resizable=yes,scrollbars=yes,width=800,height=600,screenX=10,screenY=10" );
+function previewWindow(url){
+    newWindow = window.open(url, "displayWindow", "resizable=yes,scrollbars=yes,width=800,height=600,screenX=10,screenY=10");
 }
 
-function diffWindow(url) {
-    newWindow = window.open( url, "displayWindow", "resizable=yes,scrollbars=yes,width=750,height=450,screenX=30,screenY=30" );
+function diffWindow(url){
+    newWindow = window.open(url, "displayWindow", "resizable=yes,scrollbars=yes,width=750,height=450,screenX=30,screenY=30");
 }
 
-function openDocWindow(topic) {
-    docWindow = window.open( "http://xims.info/documentation/users/xims-user_s-reference.sdbk#" + escape(topic), "displayWindow", "resizable=yes,scrollbars=yes,width=800,height=480,screenX=100,screenY=300" );
+function openDocWindow(topic){
+    docWindow = window.open("http://xims.info/documentation/users/xims-user_s-reference.sdbk#" + escape(topic), "displayWindow", "resizable=yes,scrollbars=yes,width=800,height=480,screenX=100,screenY=300");
 }
 
-function createFilterWindow(url) {
-    newWindow = window.open( url, "displayWindow", "resizable=no,scrollbars=no,width=660,height=575,screenX=10,screenY=10" );
+function createFilterWindow(url){
+    newWindow = window.open(url, "displayWindow", "resizable=no,scrollbars=no,width=660,height=575,screenX=10,screenY=10");
 }
 
-function getParamValue( param ) {
+function getParamValue(param){
     var objQuery = new Object();
     var objQuery2 = new Object();
     var windowURL;
@@ -65,21 +62,21 @@ function getParamValue( param ) {
             objQuery2[unescape(pair2[0])] = unescape(pair2[1]);
         }
     }
-
+    
     var hls = objQuery[param] ? objQuery[param] : objQuery2[param];
     return hls;
 }
 
 var highlighted = false;
-function stringHighlight( mystring ) {
-    if (highlighted || !mystring ) {
+function stringHighlight(mystring){
+    if (highlighted || !mystring) {
         return;
     }
     re = /\s+/;
     var splitted = mystring.split(re);
     var highlightStart = '<span name="highlighted" style="background: yellow">';
     var highlightEnd = '</span>';
-
+    
     for (var i in splitted) {
         var body = document.getElementById("body");
         var content = body.innerHTML;
@@ -88,9 +85,9 @@ function stringHighlight( mystring ) {
         var searchTerm = splitted[i];
         var j = -1;
         var lcSearchTerm = searchTerm.toLowerCase();
-
-        while ( content.length > 0 ) {
-            j = lcContent.indexOf(lcSearchTerm, j+1);
+        
+        while (content.length > 0) {
+            j = lcContent.indexOf(lcSearchTerm, j + 1);
             if (j < 0) {
                 newContent += content;
                 content = '';
@@ -99,7 +96,7 @@ function stringHighlight( mystring ) {
                 // skip anything inside a tag
                 if (content.lastIndexOf(">", j) >= content.lastIndexOf("<", j)) {
                     // skip anything inside scripts
-                    if ( lcContent.lastIndexOf("/script>", j) >= lcContent.lastIndexOf("<script", j) ) {
+                    if (lcContent.lastIndexOf("/script>", j) >= lcContent.lastIndexOf("<script", j)) {
                         newContent += content.substring(0, j) + highlightStart + content.substr(j, searchTerm.length) + highlightEnd;
                         content = content.substr(j + searchTerm.length);
                         lcContent = content.toLowerCase();
@@ -110,13 +107,13 @@ function stringHighlight( mystring ) {
         }
         body.innerHTML = newContent;
     }
-
+    
     highlighted = true
 }
 
-function toggleHighlight( hls ) {
+function toggleHighlight(hls){
     var cssText;
-    if ( highlighted == false ) {
+    if (highlighted == false) {
         cssText = 'background: yellow';
         highlighted = true;
     }
@@ -124,21 +121,22 @@ function toggleHighlight( hls ) {
         cssText = '';
         highlighted = false;
     }
-
+    
     var els;
     var i;
-    if ( document.all ) {
+    if (document.all) {
         els = document.getElementsByTagName("span");
-        for(i = 0;i<els.length;i++){
-            if ( els[i].name == 'highlighted' ) {
+        for (i = 0; i < els.length; i++) {
+            if (els[i].name == 'highlighted') {
                 els[i].style.cssText = cssText;
             }
         }
     }
     else {
         els = document.getElementsByName('highlighted');
-        for(i = 0;i<els.length;i++){
+        for (i = 0; i < els.length; i++) {
             els[i].style.cssText = cssText;
         }
     }
 }
+
