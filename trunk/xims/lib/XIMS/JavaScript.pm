756b485c79ebe7efb2d8872cc6fa650f37f397f5
hajn <hajn@118f944d-ca0e-0410-82e1-b214406b748f>
2011-07-26 15:03:24 +0000

    modified to support CodeMirror 1.0 syntax highlighting

--- trunk/xims/lib/XIMS/JavaScript.pm
+++ trunk/xims/lib/XIMS/JavaScript.pm
@@ -22,38 +22,43 @@ This module bla bla
 package XIMS::JavaScript;
 
 use strict;
-use base qw( XIMS::Text );
+use base qw( XIMS::Text XIMS::Object);
 use XIMS::DataFormat;
 
 our ($VERSION) = ( q$Revision$ =~ /\s+(\d+)\s*$/ );
 
+sub new {
+    my $proto = shift;
+    my $class = ref( $proto ) || $proto;
+    my %args = @_;
+
+    if ( not ( defined($args{path}) or defined($args{id}) or defined($args{document_id}) ) ) {
+        $args{data_format_id} = XIMS::DataFormat->new( name => 'ECMA' )->id() unless defined $args{data_format_id};
+    }
+
+    return $class->SUPER::new( %args );
+}
 
 
 =head2    XIMS::JavaScript->new( %args )
 
 =head3 Parameter
 
-    $args{ User }                  :  XIMS::User instance
-    $args{ path }                  :  Location path to a XIMS Object,
-                                      For example: '/xims'
-    $args{ $object_property_name } :  Object property like 'id', 'document_id',
-                                      or 'title'. To fetch existing objects
-                                      either 'path', 'id' or 'document_id' has
-                                      to be specified. Multiple object properties
-                                      can be specified in the %args hash. For
-                                      example: XIMS::JavaScript->new( id => $id )
+    %args: recognized keys are the fields from ...
 
 =head3 Returns
 
-    $javascript: Instance of XIMS::JavaScript
+    $js: XIMS::JS instance
 
 =head3 Description
 
-Fetches existing objects or creates a new instance of XIMS::JavaScript for
-object creation. Args are optional.
+Constructor
 
 =cut
 
+=head2 new()
+
+=cut
 
 sub new {
     my $proto = shift;
