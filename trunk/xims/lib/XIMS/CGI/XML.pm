ccae7530718c6c6799d02eeed87873b1068cebd0
hajn <hajn@118f944d-ca0e-0410-82e1-b214406b748f>
2011-07-26 15:17:41 +0000

    modified to support CodeMirror 1.0 syntax highlighting

--- trunk/xims/lib/XIMS/CGI/XML.pm
+++ trunk/xims/lib/XIMS/CGI/XML.pm
@@ -50,7 +50,7 @@ sub registerEvents {
           test_wellformedness
           pub_preview
           bxeconfig
-          simpleformedit
+          #simpleformedit
           unescapebody
           )
         );
@@ -98,6 +98,35 @@ sub event_unescapebody {
     return 0;
 }
 
+=head2 event_create()
+
+=cut
+
+sub event_create {
+    XIMS::Debug( 5, "called" );
+    my ( $self, $ctxt) = @_;
+
+    # event edit in SUPER implements operation control
+    $self->SUPER::event_create( $ctxt );
+    return 0 if $ctxt->properties->application->style eq 'error';
+
+    # check if a WYSIWYG Editor is to be used based on cookie or config
+    my $ed = $self->_set_wysiwyg_editor( $ctxt );
+    $ctxt->properties->application->style( "create" . $ed );
+	# $ctxt->properties->application->style( "create" );
+
+    # look for inherited CSS assignments
+    if ( not defined $ctxt->object->css_id() ) {
+        my $css = $ctxt->object->css;
+        $ctxt->object->css_id( $css->id ) if defined $css;
+    }
+
+    $self->resolve_content( $ctxt, [ qw( CSS_ID ) ] );
+
+    return 0;
+}
+
+
 =head2 event_edit()
 
 =cut
@@ -107,26 +136,36 @@ sub event_edit {
     my ( $self, $ctxt) = @_;
 
     # expand the attributes to XML-nodes
-    $self->expand_attributes( $ctxt );
+    #$self->expand_attributes( $ctxt );
 
-    # resolve document_ids to location_path after attributes have been expanded,
-    # because bxeconfig_id is stored in the attributes
-    $self->resolve_content( $ctxt, [ qw( STYLE_ID CSS_ID SCHEMA_ID BXECONFIG_ID ) ] );
+	$ctxt->properties->content->escapebody( 1 );
 
+	# event edit in SUPER implements operation control
     $self->SUPER::event_edit( $ctxt );
     return 0 if $ctxt->properties->application->style() eq 'error';
+	
+	# check if a WYSIWYG Editor is to be used based on cookie or config
+    my $ed = $self->_set_wysiwyg_editor( $ctxt );
+    $ctxt->properties->application->style( "edit" . $ed );
+	# $ctxt->properties->application->style( "edit" );
 
-    if ( XIMS::XMLEDITOR() eq 'bxe' ) {
-        $self->param( -name=>"bxepresent", -value=>"1" );
-    }
+    #if ( XIMS::XMLEDITOR() eq 'bxe' ) {
+    #   $self->param( -name=>"bxepresent", -value=>"1" );
+    #}
 
-    if ( $self->param( "edit" ) eq "bxe" ) {
-        $ctxt->properties->application->style( "edit_bxe" );
-    }
-
-    $ctxt->properties->content->escapebody( 1 );
-    return 0;
+    #if ( $self->param( "edit" ) eq "bxe" ) {
+    #    $ctxt->properties->application->style( "edit_bxe" );
+    #}
+	
+	# resolve document_ids to location_path after attributes have been expanded,
+    # because bxeconfig_id is stored in the attributes
+    #$self->resolve_content( $ctxt, [ qw( STYLE_ID CSS_ID SCHEMA_ID BXECONFIG_ID ) ] );
+	$self->resolve_content( $ctxt, [ qw( CSS_ID ) ] );
+    
+	return 0;
 }
+
+
 
 =head2 event_store()
 
@@ -213,6 +252,7 @@ sub event_store {
 
     return $self->SUPER::event_store( $ctxt );
 }
+
 
 =head2 update_decl_encoding()
 
@@ -537,11 +577,7 @@ sub event_simpleformedit {
 
 =head2 private functions/methods
 
-=over
-
 =item _updateIDs()
-
-=back
 
 =cut
 
@@ -555,6 +591,48 @@ sub _updateIDs {
     foreach my $e ( $root->findnodes("//$elementname") ) {
         $e->setAttribute( 'id', $maxid-- );
     }
+}
+
+=item _set_wysiwyg_editor()
+
+=cut
+
+sub _set_wysiwyg_editor {
+    my ( $self, $ctxt ) = @_;
+
+    my $cookiename = 'xims_wysiwygeditor';
+    my $editor = $self->cookie($cookiename);
+	# OVERRIDE for TESTING
+	# $editor = 'codemirror';
+    my $plain = $self->param( 'plain' );
+    if ( $plain or defined $editor and $editor eq 'plain' ) {
+        # $editor = undef;
+		$editor = 'codemirror';
+    }
+    elsif ($editor eq 'wepro') { 
+        # we just dumped eWebEditPro, now change the remaining cookies
+        # $editor = 'tinymce';
+    	$editor = 'codemirror';
+	}
+	elsif ($editor eq 'tinymce') {
+		# apply codemirror for syntax highlighting
+		$editor = 'codemirror';
+	}
+    elsif ( not(length $editor) and length XIMS::DEFAULTXHTMLEDITOR() ) {
+        # $editor = lc( XIMS::DEFAULTXHTMLEDITOR() );
+		$editor = 'codemirror';
+		if ( $self->user_agent('Gecko') or not $self->user_agent('Windows') ) {
+             # $editor = 'tinymce';
+			 $editor = 'codemirror';
+        }
+        my $cookie = $self->cookie( -name    => $cookiename,
+                                    -value   => $editor,
+                                    -expires => "+2160h"); # 90 days
+        $ctxt->properties->application->cookie( $cookie );
+    }
+    my $ed = '';
+    $ed = "_" . $editor if defined $editor;
+    return $ed;
 }
 
 
