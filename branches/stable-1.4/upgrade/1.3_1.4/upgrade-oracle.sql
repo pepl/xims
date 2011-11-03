e47db65b79c7625e0d3077fb30ebc27f2b26bd0d
susannetober <susannetober@118f944d-ca0e-0410-82e1-b214406b748f>
2011-11-03 13:52:13 +0000

    add missing data format 'Gallery'

--- branches/stable-1.4/upgrade/1.3_1.4/upgrade-oracle.sql
+++ branches/stable-1.4/upgrade/1.3_1.4/upgrade-oracle.sql
@@ -1,3 +1,5 @@
 PROMPT Adding new object types
 INSERT INTO CI_OBJECT_TYPES ( id, name, is_fs_container, is_xims_data, redir_to_self, parent_id, publish_gopublic, is_objectroot, is_davgetable, davprivval, is_mailable  )
   VALUES ( OBT_SEQ.NEXTVAL, 'Gallery',  1, 1, 0, null, 0, 0, 1, 1, 0  );
+INSERT INTO CI_DATA_FORMATS ( id, name, mime_type, suffix  )
+  VALUES ( 49, 'Gallery',  'application/x-container', null  );
--- branches/stable-1.4/upgrade/1.3_1.4/upgrade-pl.sql
+++ branches/stable-1.4/upgrade/1.3_1.4/upgrade-pl.sql
@@ -1,3 +1,5 @@
 \echo adding new VLibrary ObjectTypes
 INSERT INTO ci_object_types ( id, name, is_fs_container, is_xims_data, redir_to_self, parent_id, publish_gopublic, is_objectroot, is_davgetable, davprivval, is_mailable)
        VALUES ( nextval('ci_object_types_id_seq'), 'Gallery',  1, 1, 0, null, 0, 0, 1, 1, 0  );
+INSERT INTO CI_DATA_FORMATS ( id, name, mime_type, suffix  )
+  VALUES ( 49, 'Gallery',  'application/x-container', null  );
