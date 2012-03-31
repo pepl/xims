3baae6b556836d6f827ca147702134cd7fe627bb
Michael Kröll <pepl@cpan.org>
2012-03-31 09:58:47 +0000

    Fixed broken SQL

--- xims/upgrade/1.4_2.0/upgrade-pg.sql
+++ xims/upgrade/1.4_2.0/upgrade-pg.sql
@@ -1,8 +1,8 @@
 DELETE FROM CI_OBJECT_TYPES WHERE NAME IN ('AnonDiscussionForum', 'AnonDiscussionForumContrib');
 ALTER TABLE CI_OBJECT_TYPES ADD MENU_LEVEL SMALLINT DEFAULT 0;
-UPDATE TABLE CI_OBJECT_TYPES SET MENU_LEVEL=1 WHERE NAME IN 
-	('Document', 'NewsItem', 'NewsItem2', 'Newsletter', 'Folder', 'Departmentroot', 'File', 'Image', 'Gallery', 'URLLink', 'Portlet')
+UPDATE CI_OBJECT_TYPES SET MENU_LEVEL=1 WHERE NAME IN 
+	('Document', 'Folder', 'Departmentroot', 'File', 'Image', 'URLLink')
 	AND PARENT_ID IS NULL;
-UPDATE TABLE CI_OBJECT_TYPES SET MENU_LEVEL=2 WHERE NAME IN 
-	('ReferenceLibrary', 'Questionnaire', 'XML', 'sDocBookXML', 'SQLReport', 'Javascript', 'AxPointPresentation', 'SymbolicLink', 'XSPScript', 'CSS', 'VLibrary', 'XSLStylesheet', 'TAN_List', 'SimpleDB', 'Text')
+UPDATE CI_OBJECT_TYPES SET MENU_LEVEL=2 WHERE NAME IN 
+	('Portlet', 'NewsItem', 'Gallery', 'Newsletter', 'NewsItem2', 'ReferenceLibrary', 'Questionnaire', 'XML', 'sDocBookXML', 'SQLReport', 'Javascript', 'AxPointPresentation', 'SymbolicLink', 'XSPScript', 'CSS', 'VLibrary', 'XSLStylesheet', 'TAN_List', 'SimpleDB', 'Text', 'AnonDiscussionForum')
 	AND PARENT_ID IS NULL;
