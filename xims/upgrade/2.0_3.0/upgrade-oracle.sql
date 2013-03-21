3608bea3b3dc3e6f77c81ce8020fe1aa2f9e1c48
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2013-03-21 08:08:55 +0000

    + handle non-empty tables.

--- xims/upgrade/2.0_3.0/upgrade-oracle.sql
+++ xims/upgrade/2.0_3.0/upgrade-oracle.sql
@@ -1,3 +1,5 @@
 PROMPT Extending ci_sessions
-ALTER TABLE CI_SESSIONS ADD (auth_module VARCHAR2(50)) NOT NULL
+ALTER TABLE CI_SESSIONS ADD auth_module VARCHAR2(50);
+UPDATE CI_SESSIONS SET auth_module = 'dummy_value';
+ALTER TABLE CI_SESSIONS MODIFY auth_module NOT NULL
 /
