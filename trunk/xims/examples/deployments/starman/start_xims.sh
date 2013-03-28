26df26ece96ec45faeac5ee5d977e4bdbde64ea8
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2013-03-28 14:20:25 +0000

    + renamed and completed start script.

--- trunk/xims/examples/deployments/starman/start_xims.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This is just an example, adapt to your needs.
-export XIMS_HOME=/opt/xims
-export PERLBREW_ROOT=/opt/perl5
-source ${PERLBREW_ROOT}/etc/bashrc
-cd "${XIMS_HOME}/lib"
-start_server --port 127.0.0.1:5000 -- starman --workers 12 xims.psgi > ${XIMS_HOME}/logs/error_log 2>&1 
