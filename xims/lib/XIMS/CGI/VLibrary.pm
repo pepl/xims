0e195b2530c4feca1746108fb536eab88253196d
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2013-07-17 08:17:07 +0000

    + reflect changes for set_wysiwyg_editor().

--- xims/lib/XIMS/CGI/VLibrary.pm
+++ xims/lib/XIMS/CGI/VLibrary.pm
@@ -490,8 +490,7 @@ sub event_property_edit {
     my $ed = '';
 
     if ($property eq 'subject') {
-        # XXX calling foreign internal methods is certainly not not ugly...
-        $ed = XIMS::CGI::Document::_set_wysiwyg_editor($self, $ctxt);
+        $ed = $self->set_wysiwyg_editor($self, $ctxt);
         # either Plain or HTMLArea
         if (length($ed)) {$ed = '_tinymce';}
     }
