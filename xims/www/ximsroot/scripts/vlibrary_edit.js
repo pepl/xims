c917cd683894018eb2558c977717f7cf5d15f4d7
Susanne Tober <375527+susannetober@users.noreply.github.com>
2013-08-02 13:11:56 +0000

    js cleanup

--- xims/www/ximsroot/scripts/vlibrary_edit.js
+++ xims/www/ximsroot/scripts/vlibrary_edit.js
@@ -62,7 +62,179 @@ function createMapping ( property, text ) {
     return false;
 }
 
+function initVLibMenu(){
+	$("#vlib-menu").button({
+			icons: {
+				secondary: "ui-icon-triangle-1-s"
+			}
+		}).each(function() {
+			$(this).next().menu({
+				select: function(event, ui) {
+					$(this).hide();
+					window.location = ui.item.children('a').attr('href');
+				},
+				input: $(this)
+			}).hide();
+		}).click(function(event) {
+			var menu = $(this).next();
+			if (menu.is(":visible")) {
+				menu.hide();
+				return false;
+			}
+			menu.menu().show().css({top:0, left:0}).position({
+        my: "left top",
+        at: "left bottom",
+        of: this
+      });
+			/*
+			menu.menu("deactivate").show().css({top:0, left:0}).position({
+				my: "left top",
+				at: "left bottom",
+				of: this
+			});
+			*/
+			$(document).one("click", function() {
+				menu.hide();
+			});
+			return false;
+		});
+}
+
+function addProperty( property, fullname ) {
+    //original = eval( "document.eform.vl" + property );
+	original = "document.eform.vl" + property;
+    newvalue = fullname;
+    if ( original.value.length > 0 ) {
+        values = original.value.split(';');
+        values.push(newvalue);
+        original.value = values.join(';');
+    }
+    else {
+        original.value = newvalue;
+    }
+}
+
+function initPropDescription(property){
+	var pos = findPos($(property).prev());
+	var inputfield = property.substr(property.indexOf('_')+1);
+	inputfield = 'input-'+inputfield; 
+	var offset = document.getElementById(inputfield).offsetWidth;
+	$(property).css('left',pos[0]+offset+10);
+	$(property).css('top',pos[1]);
+	$(property).css('display', 'block');
+}
+function destroyInfoBox(self){
+	$(self).css('display', 'none');
+}
+	
+function initAuthDescription(auth_desc, inputfield){
+	var neighbour = $(auth_desc).prev();
+	while (neighbour && (neighbour.type == 'hidden' || neighbour.hasClass('hidden-content'))){
+		neighbour = neighbour.prev();
+	}
+	var pos = findPos(neighbour);
+	var offset = getTotalWidth(neighbour); 
+
+	$(auth_desc).css('left',pos[0]+offset+10);
+	$(auth_desc).css('top',pos[1]);
+	$(auth_desc).css('display', 'block');
+}
+
 function removeMapping ( property, property_id ) {
     post_async("remove_mapping_async=1;property=" + property
                + ";property_id=" + property_id, property);
 }
+
+function refresh( property ) {
+    $("#svl" + property + "container").load(parentpath + '?list_properties_items=1;property=' + property);
+
+} 
+
+function mkHandleMapResponse(data, property) {
+    var mapped_properties = 'mapped_' +  property + 's';
+	$('#' + mapped_properties).html(data); 	
+}
+
+function createMapping ( property, value, text ) {
+    if ( value > 0 ) {
+		var jsonQuery = {
+			create_mapping_async: "1",
+			property: property,
+			property_id: value
+		};
+        var jsonObject = post_async(jsonQuery, property);	
+		mkHandleMapResponse(jsonObject, property);					   
+    }
+    else {
+        alert ( text );
+    }
+    //return false;
+}
+
+function removeMapping ( property, property_id ) {
+	var jsonQuery = {
+		remove_mapping_async: "1",
+		property: property,
+		property_id: property_id
+	};
+	post_async(jsonQuery, property);
+	
+}
+
+function deleteProperty( property, property_id ){
+	var jsonQuery = {
+		property_delete: "1",
+		property: property,
+		property_id: property_id
+	};
+	$.ajax({
+		type: 'GET',
+		url: abspath,
+		dataType: 'json',
+		data: jsonQuery,
+		success: function(jsonObject){
+		}
+	});
+	document.location.replace(abspath);
+	//var jsonObject = post_async(jsonQuery, property);
+	
+}
+
+$(document).ready(function(){
+	
+	//initCreateMenu();
+	initVLibMenu();
+
+	$("#svlsubject").combobox({
+		input_id: "vlsubject",
+		button_type: "button",
+		input_size: '43'
+	});
+	
+	$("#svlkeyword").combobox({
+		input_id: "vlkeyword",
+		button_type: "button",
+		input_size: '43'
+	});
+	$("#svlauthor").combobox({
+		input_id: "vlauthor",
+		button_type: "button",
+		input_size: '43'
+	});
+	$("#svlpublication").combobox({
+		input_id: "vlpublication",
+		button_type: "button",
+		input_size: '43'
+	});
+	$("#svleditor").combobox({
+		input_id: "vleditor",
+		button_type: "button",
+		input_size: '43'
+	});
+	$("#svlserial").combobox({
+		input_id: "vlserial",
+		button_type: "button",
+		input_size: '43'
+	});
+});
+
