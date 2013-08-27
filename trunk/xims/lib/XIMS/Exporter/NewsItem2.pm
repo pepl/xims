3bfddee8ce853cd24c7e297284a20cd8d020f13f
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2013-08-27 14:23:52 +0000

    + add missing Exporter::NewsItem2

---
+++ trunk/xims/lib/XIMS/Exporter/NewsItem2.pm
@@ -0,0 +1,42 @@
+
+=head1 XIMS::Exporter::NewsItem2
+
+lowlevel class for newsitem objs.
+
+=cut
+
+package XIMS::Exporter::NewsItem2;
+
+use common::sense;
+use parent qw( XIMS::Exporter::Document2 );
+
+use XIMS::SAX::Filter::ContentIDPathResolver;
+use XIMS::SAX::Filter::ContentObjectPropertyResolver;
+
+sub set_sax_filters {
+    XIMS::Debug( 5, "called" );
+    my $self   = shift;
+    my @retval = ();
+
+    push @retval, XIMS::SAX::Filter::ContentIDPathResolver->new(
+        Provider       => $self->{Provider},
+        ResolveContent => [
+            qw(
+                department_id
+                symname_to_doc_id
+                )
+        ]
+        ),
+        XIMS::SAX::Filter::ContentObjectPropertyResolver->new(
+        User           => $self->{User},
+        ResolveContent => [qw( image_id )],
+        Properties     => [qw( abstract data_format_name )]
+        );
+
+    $self->{Options}->{appendexportfilters} = 1;
+
+    return @retval;
+}
+
+1;
+
