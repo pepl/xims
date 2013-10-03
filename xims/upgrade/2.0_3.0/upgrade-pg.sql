a8721aec4b88399cadbf1c6220d7c772b7332a1b
haensel <haensel@118f944d-ca0e-0410-82e1-b214406b748f>
2013-10-03 09:44:41 +0000

    + check constraint ci_documents.parent_id != ci_documents.id.

--- xims/upgrade/2.0_3.0/upgrade-pg.sql
+++ xims/upgrade/2.0_3.0/upgrade-pg.sql
@@ -2,3 +2,5 @@
 ALTER TABLE CI_SESSIONS ADD AUTH_MODULE VARCHAR(50);
 UPDATE CI_SESSIONS SET auth_module = 'dummy_value';
 ALTER TABLE CI_SESSIONS ALTER COLUMN auth_module SET NOT NULL;
+\echo Adding check parent_id != id on ci_documents
+ALTER TABLE CI_DOCUMENTS ADD CONSTRAINT DOC_PARENT_ISNT_DOC CHECK (parent_id != id);
