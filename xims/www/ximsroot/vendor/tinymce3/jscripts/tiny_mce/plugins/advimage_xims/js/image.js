6260946618affbef3e8bbf375317333e08d229a1
Susanne Tober <375527+susannetober@users.noreply.github.com>
2014-01-21 12:05:11 +0000

    fixed bug on styling tinymce image/caption

--- xims/www/ximsroot/vendor/tinymce3/jscripts/tiny_mce/plugins/advimage_xims/js/image.js
+++ xims/www/ximsroot/vendor/tinymce3/jscripts/tiny_mce/plugins/advimage_xims/js/image.js
@@ -208,6 +208,21 @@ var ImageDialog = {
 		});
 
 		el = ed.selection.getNode();
+		
+		var div;
+		if(el.parentNode.className.indexOf('caption') != -1){
+			div = el.parentNode;
+			tinymce.extend(args, {
+				src : nl.src.value,
+				width : nl.width.value,
+				height : nl.height.value,
+				alt : nl.alt.value,
+				title : nl.title.value,			
+				style : '',			
+				'class' : imgclass // classes for image popups - IE needs the quotes around class
+			});
+			ed.dom.setAttribs(div, {style: nl.style.value+' width: '+nl.width.value+'px'});
+		}
 
 		if (el && el.nodeName == 'IMG') {
 			ed.dom.setAttribs(el, args);
